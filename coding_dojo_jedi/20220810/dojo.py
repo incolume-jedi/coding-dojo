e71bfb1a6bd250207c197e88021e46d6d5a75345
alfredogust <106781269+alfredogust@users.noreply.github.com>
2022-08-10 21:31:10 -0300

    soluçoes do dojo de vogais 20220810 (#33)

---
+++ coding_dojo_jedi/20220810/dojo.py
@@ -0,0 +1,19 @@
+def is_vogal0(letra: str) -> bool:
+    vogais = ['a', 'e', 'i', 'o', 'u']
+    for vogal in vogais:
+        if letra == vogal:
+            return True
+
+    return False
+
+
+def is_vogal1(letra: str) -> bool:
+    vogais = ['a', 'e', 'i', 'o', 'u']
+    if letra in vogais:
+        return True
+    return False
+
+
+def is_vogal(letra: str) -> bool:
+    vogais = {'a':True, 'e':True, 'i':True, 'o':True, 'u':True}
+    return vogais.get(letra, False)

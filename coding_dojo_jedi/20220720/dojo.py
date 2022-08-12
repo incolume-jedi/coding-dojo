db961bceb0524c49e34146393a64bd21d1c35a5e
Britodfbr <britodfbr@gmail.com>
2022-08-12 12:59:59 -0300

    Solução para dojo de 20-07-2022
    Recuperado.
    
    close #32

---
+++ coding_dojo_jedi/20220720/dojo.py
@@ -0,0 +1,32 @@
+def calculadora(op: str, x: (int|float), y: (int|float))->float:
+    result = 0
+    operadores = '+ - * ** / // %'.split()
+    if op not in operadores:
+        raise ValueError(f'Operador inválido. Use: {" ".join(operadores)}')
+
+    try:
+        x = float(x)
+        y = float(y)
+    except :
+        raise ValueError('x e y devem ser valores numéricos reais.')
+
+    try:
+        if op == '+':
+            result = x + y
+        if op == '-':
+            result = x - y
+        if op == '*':
+            result = x * y
+        if op == '/':
+            result = x / y
+        if op == '%':
+            result = x % y
+        if op == '//':
+            result = x // y
+        if op == '**':
+            result = x ** y
+    except ZeroDivisionError:
+        raise ValueError('y deve ser diferente 0.')
+
+    return result
+

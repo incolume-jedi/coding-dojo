ad58e1e7cd3f762324c249bc5a318251cf317973
alfredogust <alfredogustavo.019@gmail.com>
2022-08-31 21:30:31 -0300

    solução do problema 1

--- coding_dojo_jedi/20220831/problema1/tests.py
+++ coding_dojo_jedi/20220831/problema1/tests.py
@@ -5,11 +5,11 @@ from dojo import conceito
 @pytest.mark.parametrize(
     'entrance expected'.split(),
     (
-        ((10, 10), 'Média 10, "A", APROVADO'),
-        ((1, 1), 'Média 1, "E", REPROVADO'),
+        ((10, 10), 'Média 10.0, "A", APROVADO'),
+        ((1, 1), 'Média 1.0, "E", REPROVADO'),
         ((10, 5), 'Média 7.5, "B", APROVADO'),
-        ((9, 5), 'Média 7, "C", APROVADO'),
-        ((6, 4), 'Média 5, "D", REPROVADO'),
+        ((9, 5), 'Média 7.0, "C", APROVADO'),
+        ((6, 4), 'Média 5.0, "D", REPROVADO'),
     )
 )
 def test_conceito(entrance, expected):

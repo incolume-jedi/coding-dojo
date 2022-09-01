c28a953982c12cd342f926fea050258b03269c8d
Britodfbr <britodfbr@gmail.com>
2022-08-31 21:00:02 -0300

    test: Correção do parametros de teste

--- coding_dojo_jedi/20220831/problema1/tests.py
+++ coding_dojo_jedi/20220831/problema1/tests.py
@@ -5,12 +5,12 @@ from dojo import conceito
 @pytest.mark.parametrize(
     'entrance expected'.split(),
     (
-        ((10, 10) == 'Média 10, "A", APROVADO'),
-        ((1, 1) == 'Média 1, "E", REPROVADO'),
-        ((10, 5) == 'Média 7.5, "B", APROVADO'),
-        ((9, 5) == 'Média 7, "C", APROVADO'),
-        ((6, 4) == 'Média 5, "D", REPROVADO'),
+        ((10, 10), 'Média 10, "A", APROVADO'),
+        ((1, 1), 'Média 1, "E", REPROVADO'),
+        ((10, 5), 'Média 7.5, "B", APROVADO'),
+        ((9, 5), 'Média 7, "C", APROVADO'),
+        ((6, 4), 'Média 5, "D", REPROVADO'),
     )
 )
 def test_conceito(entrance, expected):
-    assert conceito(entrance) == expected
+    assert conceito(*entrance) == expected

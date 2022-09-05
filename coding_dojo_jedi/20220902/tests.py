5c8b7d72201d811a10c2f1c749506beb235a99ee
Britodfbr <britodfbr@gmail.com>
2022-09-05 08:30:43 -0300

    test: Testes do original acrescentados
    disponível em https://github.com/incolume-jedi/coding-dojo/blob/main/coding_dojo_jedi/20190129/tests.py

--- coding_dojo_jedi/20220902/tests.py
+++ coding_dojo_jedi/20220902/tests.py
@@ -1,6 +1,42 @@
 import pytest
 from dojo import trocar_dinheiro
 
+@pytest.mark.skip("skiped..")
+def test_moedas_150():
+    (notas, moedas) = trocar_dinheiro(1.5)
+    assert notas == [0, 0, 0, 0, 0, 0, 0]
+    assert moedas == [1, 1, 0, 0, 0, 0]
+
+@pytest.mark.skip("skiped..")
+def test_moedas_191():
+    (notas, moedas) = trocar_dinheiro(1.91)
+    assert notas == [0, 0, 0, 0, 0, 0, 0]
+    assert moedas == [1, 1, 1, 1, 1, 1]
+
+@pytest.mark.skip("skiped..")
+def test_moedas_192():
+    (notas, moedas) = trocar_dinheiro(1.92)
+    assert notas == [0, 0, 0, 0, 0, 0, 0]
+    assert moedas == [1, 1, 1, 1, 1, 2]
+
+@pytest.mark.skip("skiped..")
+def test_notas_101():
+    (notas, moedas) = trocar_dinheiro(101)
+    assert notas == [0, 1, 0, 0, 0, 0, 0]
+    assert moedas == [1, 0, 0, 0, 0, 0]
+
+@pytest.mark.skip("skiped..")
+def test_notas_101_92():
+    (notas, moedas) = trocar_dinheiro(101.92)
+    assert notas == [0, 1, 0, 0, 0, 0, 0]
+    assert moedas == [1, 1, 1, 1, 1, 2]
+
+@pytest.mark.skip("skiped..")
+def test_notas_0():
+    (notas, moedas) = trocar_dinheiro(0.0)
+    assert notas == [0, 0, 0, 0, 0, 0, 0]
+    assert moedas == [0, 0, 0, 0, 0, 0]
+
 
 @pytest.mark.parametrize(
     ('entrance', 'expected'),

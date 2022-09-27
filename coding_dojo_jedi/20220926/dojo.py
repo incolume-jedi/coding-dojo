763030e900bb40e376196b113621778e5a66b08c
Britodfbr <britodfbr@gmail.com>
2022-09-26 21:33:38 -0300

    Aplicado boas praticas de desenvolvimento; Retirado função aninhada

--- coding_dojo_jedi/20220926/dojo.py
+++ coding_dojo_jedi/20220926/dojo.py
@@ -1,3 +1,9 @@
+def aminoacid(string: str, codons: dict) -> str:
+    if len(string) <= 3:
+        return codons.get(string)
+    return '{}{}'.format(codons.get(string[:3]), aminoacid(string[3:], codons))
+
+
 def protein(chain: str) -> str:
     codons = {
         'UUC': 'F', 'UUU': 'F',
@@ -23,14 +29,7 @@ def protein(chain: str) -> str:
         # 'UAA': 'Stop', 'UGA': 'Stop', 'UAG': 'Stop',
         'UAA': '', 'UGA': '', 'UAG': '',
     }
-    result = ''
-
-    def aminoacid(string: str):
-        if len(string) <= 3:
-            return codons.get(string)
-        return '{}{}'.format(codons.get(string[:3]), aminoacid(string[3:]))
-
-    return aminoacid(chain.upper())
+    return aminoacid(chain.upper(), codons)
 
 
 if __name__ == '__main__':    # pragma: no cover

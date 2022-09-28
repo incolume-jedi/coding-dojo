ddfdaef1da16d39692afc0cf881711162bf13919
Britodfbr <britodfbr@gmail.com>
2022-09-28 20:51:34 -0300

    Tratativa de solucionar problema
    Não resolvido

---
+++ coding_dojo_jedi/20220928/dojo.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+from typing import Container, List, Set, Tuple
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+def max_sequence(container: (List | Set | Tuple | Container)):
+    result = 0
+    if not container or all(x < 0 for x in container):
+        return result
+
+    for i in container:
+        result += i
+        if result >= 0:
+            ...
+        else:
+            result = 0
+
+    return result

69706f51da180a905b935594577d93cdd7c4979e
Britodfbr <britodfbr@gmail.com>
2023-09-13 09:28:55 -0300

    fix: #0 Conformidade de teste conforme exemplo README

--- coding_dojo_jedi/20220910/tests.py
+++ coding_dojo_jedi/20220910/tests.py
@@ -3,17 +3,18 @@ from dojo import weekday
 
 
 @pytest.mark.parametrize(
-    'entrance expected'.split(),
+    "entrance expected".split(),
     (
-        (1, 'Domingo'),
-        (2, 'Segunda'),
-        (3, 'Terça'),
-        (4, 'Quarta'),
-        (5, 'Quinta'),
-        (6, 'Sexta'),
-        (7, 'Sábado'),
-        (8, 'Valor inválido'),
-        (9, 'Valor inválido'),
+        (1, "Domingo"),
+        (2, "Segunda"),
+        (3, "Terça"),
+        (4, "Quarta"),
+        (5, "Quinta"),
+        (6, "Sexta"),
+        (7, "Sábado"),
+        (8, "Valor inválido"),
+        (9, "Valor inválido"),
+        (0, "Valor inválido"),
     ),
 )
 def test_weekday(entrance, expected):

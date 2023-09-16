34bd92fcd42df6fa3e17da770f37f2939dbfc7dc
Britodfbr <britodfbr@gmail.com>
2023-09-16 09:45:27 -0300

    tests: #86 ampliando as verificações dos testes dojo 20220727

--- coding_dojo_jedi/20220727/test_20220727.py
+++ coding_dojo_jedi/20220727/test_20220727.py
@@ -128,8 +128,31 @@ from star_wars import research
         ),
         (
           'sky',
-          []
-        )
+          ['Luke Skywalker', 'Anakin Skywalker', 'Shmi Skywalker']
+        ),
+        (
+            'jinn',
+            [
+                {
+                    'birth_year': '92BBY',
+                    'created': '2014-12-19T16:54:53.618000Z',
+                    'edited': '2014-12-20T21:17:50.375000Z',
+                    'eye_color': 'blue',
+                    'films': ['https://swapi.dev/api/films/4/'],
+                    'gender': 'male',
+                    'hair_color': 'brown',
+                    'height': '193',
+                    'homeworld': 'https://swapi.dev/api/planets/28/',
+                    'mass': '89',
+                    'name': 'Qui-Gon Jinn',
+                    'skin_color': 'fair',
+                    'species': [],
+                    'starships': [],
+                    'url': 'https://swapi.dev/api/people/32/',
+                    'vehicles': ['https://swapi.dev/api/vehicles/38/']
+                },
+            ],
+         ),
     )
 )
 def test_research(entrance, expected):

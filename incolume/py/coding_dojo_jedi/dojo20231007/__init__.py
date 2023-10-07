1adff33b98053cfbb6f07bb0f3baa4a8dc43ea95
EVANGELISTA-RIBEIRO <146137984+EVANGELISTA-RIBEIRO@users.noreply.github.com>
2023-10-07 19:44:29 +0000

    refactor: #36 Resolução parcial do dojo subarray de soma máxima.

--- incolume/py/coding_dojo_jedi/dojo20231007/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20231007/__init__.py
@@ -3,7 +3,28 @@ def max_sequence(lista: list) -> int:
     """
     soma = 0
     # TODO identificar se os números são negativos
-    for i in lista:
-        soma = soma + i
-    return soma
+    resultado = []
 
+    for x in lista:
+        resultado.append(x < 0)
+
+    if all(resultado):
+        return 0
+    
+    resultado.clear()
+    for y in lista:
+        resultado.append(y >= 0)
+
+    if all(resultado):
+        soma = 0
+        for numero in lista:
+            soma = soma + numero 
+        return soma
+    #[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    # 4, -1, 2, 1,
+    #[10, -11, 2, 3, 4, 5, -5,  6, 7, -50, 8,-7, 9]
+    # 2, 3, 4, 5,
+    return resultado
+    """for i in lista:
+        soma = soma + i
+    return soma"""

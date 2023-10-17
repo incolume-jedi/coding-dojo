9bbd88595f5bb613756f1e6a729a88dc457dfb43
EVANGELISTA-RIBEIRO <146137984+EVANGELISTA-RIBEIRO@users.noreply.github.com>
2023-10-17 00:06:21 +0000

    feat: Estrutura dojo romanos para arábicos

--- incolume/py/coding_dojo_jedi/dojo20231016/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231016/dojo.py
@@ -1,3 +1,11 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = "@britodfbr"  # pragma: no cover
+
+
+def from_roman(numero_romano: str) -> int:
+    """Transforma número romanos em arábicos."""
+
+    romanos = ["I","V","X","L","C","D","M"]
+    arabicos = [1,5,10,50,100,500,1000]
+    return 0

ea71d08df2931267e4d58fd0b57ca43c1c00fc40
britodfbr <britodfbr@gmail.com>
2023-10-26 15:31:58 -0300

    Revert "refactor: #59 tratativa de uso python-markdown"
    This reverts commit 17132b300bb0263a52940bbb6d60eb7be4713444.

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -1,15 +1,11 @@
 from pathlib import Path
-from markdown import markdownFromFile
-
 
 def generator_sumary(fout: Path = None) -> Path:
     """Gerador de sumário."""
 
-    files = sorted(list(Path(__file__).parents[1].rglob('**/README.md')))
-    print(len(files))
-    fmd = files[0]
-    print(markdownFromFile(input=fmd))
-
+    files = Path(__file__).parents[1].rglob('**/README.md')
+    print(len(list(files)))
+    
     # return Path(__file__).absolute().parent.parent
     file = fout or Path(__file__).parent.joinpath('sumario.md')
     # file.write_text('')

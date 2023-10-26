790ca6f222038119c9eb7e70b21439cef3b4abca
britodfbr <britodfbr@gmail.com>
2023-10-26 18:54:10 -0300

    style: #59 correções lint aplicadas.

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -1,30 +1,34 @@
-from pathlib import Path
+"""Modulo para dojo20231025."""
 import re
+from pathlib import Path
 
 
-def generator_sumary(fout: Path = None) -> Path:
+def generator_sumary(fout: Path | None = None) -> Path:
     """Gerador de sumário."""
-
     file = fout or Path(__file__).parent.joinpath('sumario.md')
     regex = r'## Problema\s*\*\*((\w+\s*)+)\*\*'
 
-     
     with file.open('w') as fmd:
         fmd.writelines(
             [
-                '# Coding Dojo\n', 
-                '**Guilda JEDI Incolume - Grupo Python Incolume**\n', 
-                '- [Seja membro da Guilda JEDI Incolume](https://discord.gg/eBNamXVtBW)\n', 
-                '## Sumário dos dojos\n\n---\n'
-            ]
+                '# Coding Dojo\n',
+                '**Guilda JEDI Incolume - Grupo Python Incolume**\n',
+                '- [Seja membro da Guilda JEDI Incolume]'
+                '(https://discord.gg/eBNamXVtBW)\n',
+                '## Sumário dos dojos\n\n---\n',
+            ],
         )
-        for filemd in sorted(list(Path(__file__).parents[1].rglob('**/README.md'))):
+        for filemd in sorted(
+            Path(__file__).parents[1].rglob('**/README.md'),
+        ):
             try:
                 result = re.search(regex, filemd.read_text(), flags=re.I)
-                sout = f'1. [{filemd.parts[-2].capitalize()} &#8212; {result.group(1)}]({Path("..").joinpath(*filemd.parts[-2:])})\n'
-                # print(sout)
+                title = filemd.parts[-2].capitalize()
+                desc = result.group(1)  # type: ignore[union-attr]
+                link = Path('..').joinpath(*filemd.parts[-2:])
+                sout = f'1. [{title} &#8212; {desc}]({link})\n'
                 fmd.write(sout)
-            except AttributeError:
+            except AttributeError:  # noqa: PERF203
                 pass
         fmd.writelines(['\n---\n\n', '&copy; Incolume.com.br\n\n'])
     return file

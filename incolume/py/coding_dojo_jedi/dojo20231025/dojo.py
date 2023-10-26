b43a8b7a84de2c652d556162229a7cf35a35ca00
britodfbr <britodfbr@gmail.com>
2023-10-26 16:06:09 -0300

    refactor: #59 montagem de 1 link de dojo para arquivo de sumário.

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -1,11 +1,20 @@
 from pathlib import Path
+import re
+
 
 def generator_sumary(fout: Path = None) -> Path:
     """Gerador de sumário."""
 
-    files = Path(__file__).parents[1].rglob('**/README.md')
-    print(len(list(files)))
-    
+    files = sorted(list(Path(__file__).parents[1].rglob('**/README.md')))
+    print(len(files))
+    regex = r'## Problema\s*\*\*((\w+\s*)+)\*\*'
+    fmd = files[2]
+    result = re.search(regex, fmd.read_text(), flags=re.I)
+    sout = f'1. [{fmd.parts[-2]} &#8212; {result.group(1)}]({Path("..").joinpath(*fmd.parts[-2:])})'
+    print(sout)
+
+
+
     # return Path(__file__).absolute().parent.parent
     file = fout or Path(__file__).parent.joinpath('sumario.md')
     # file.write_text('')

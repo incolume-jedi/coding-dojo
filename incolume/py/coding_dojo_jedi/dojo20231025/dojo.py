6c7df2718772d274b1ef763638ee8504c3797ca0
britodfbr <britodfbr@gmail.com>
2023-10-26 15:06:31 -0300

    test: #59 atualização dos testes referentes a contagem de dojos realizados

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-def generator_sumary(fout: Path = None) -> bool:
+def generator_sumary(fout: Path = None) -> Path:
     """Gerador de sumário."""
+
+    files = Path(__file__).parents[1].rglob('**/README.md')
+    print(len(list(files)))
+    
     # return Path(__file__).absolute().parent.parent
     file = fout or Path(__file__).parent.joinpath('sumario.md')
     # file.write_text('')
-    with file.open() as f:
-        for line in f:
-            if line.startswith('1'):
-                print(line)
     return file
--- incolume/py/coding_dojo_jedi/dojo20231025/test_20231025.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/test_20231025.py
@@ -3,8 +3,19 @@ from pathlib import Path
 
 
 def test_quantia()->None:
-    """Testar quantidade de links e dojos são iguais."""
-    assert generator_sumary() == len(list(Path(__file__).absolute().parents[1].rglob('dojo*')))
+    """Testar se a quantidade de links e dojos são iguais."""
+
+    def count_links(arq_entrada: Path) -> int:
+        """Contar os links do sumario.md."""
+        contagem = 0
+        with arq_entrada.open() as f:  
+            for line in f:
+                if line.startswith('1'):
+                    contagem += 1
+        return contagem
+
+
+    assert count_links(generator_sumary()) == len(list(Path(__file__).absolute().parents[1].rglob('dojo*')))
 
 # link correto
 # escopo sumário

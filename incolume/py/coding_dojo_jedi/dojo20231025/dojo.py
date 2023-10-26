17132b300bb0263a52940bbb6d60eb7be4713444
britodfbr <britodfbr@gmail.com>
2023-10-26 15:20:13 -0300

    refactor: #59 tratativa de uso python-markdown
    python-markdown falhou na solução do problema.

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+from markdown import markdownFromFile
+
 
 def generator_sumary(fout: Path = None) -> Path:
     """Gerador de sumário."""
 
-    files = Path(__file__).parents[1].rglob('**/README.md')
-    print(len(list(files)))
-    
+    files = sorted(list(Path(__file__).parents[1].rglob('**/README.md')))
+    print(len(files))
+    fmd = files[0]
+    print(markdownFromFile(input=fmd))
+
     # return Path(__file__).absolute().parent.parent
     file = fout or Path(__file__).parent.joinpath('sumario.md')
     # file.write_text('')

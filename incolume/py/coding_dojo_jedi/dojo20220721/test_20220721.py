deb6f16ec139bd8f52d6c830e18639db673cc291
Britodfbr <britodfbr@gmail.com>
2023-10-28 20:10:15 -0300

    style: #144 Lint style aplicado

--- incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
+++ incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
@@ -1,10 +1,12 @@
+"""Unittest TDD for dojo."""
 # !/usr/bin/env python
 from os import environ
 
 from incolume.py.coding_dojo_jedi.dojo20220721.dojo20220721 import saudacao
 
 
-def test_saudacao(capsys):
+def test_saudacao(capsys) -> None:
+    """Test saudacao."""
     timeout = float(environ.get('TIMEOUT', 9))
     saudacao(timeout)
     output = capsys.readouterr()

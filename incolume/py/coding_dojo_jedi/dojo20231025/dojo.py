3f087f81f6c6d864c115d71a032096729b9f3cc8
Britodfbr <britodfbr@gmail.com>
2023-10-29 23:03:30 -0300

    fix: #144 aplicado re.escape para contornar encode

--- incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231025/dojo.py
@@ -6,17 +6,19 @@ from pathlib import Path
 def generator_sumary(fout: Path | None = None) -> Path:
     """Gerador de sumário."""
     file = fout or Path(__file__).parent.joinpath('sumario.md')
-    regex = r'## Problema\s*\*\*((\w+\s*)+)\*\*'
+    regex = re.escape('## Problema\s*\*\*((\w+\s*)+)\*\*')
 
     with file.open('w') as fmd:
         fmd.writelines(
             [
-                '# Coding Dojo\n\n',
-                '**Guilda JEDI Incolume - Grupo Python Incolume**\n\n',
-                '- [Seja membro da Guilda JEDI Incolume]'
-                '(https://discord.gg/eBNamXVtBW)\n\n',
-                '## Sumário dos dojos\n\n',
-                '---\n\n',
+                re.escape('# Coding Dojo\n\n'),
+                re.escape('**Guilda JEDI Incolume - '
+                          'Grupo Python Incolume**\n\n'),
+                re.escape(
+                    '- [Seja membro da Guilda JEDI Incolume]'
+                          '(https://discord.gg/eBNamXVtBW)\n\n'),
+                re.escape('## Sumário dos dojos\n\n'),
+                re.escape('---\n\n'),
             ],
         )
         for filemd in sorted(
@@ -33,8 +35,9 @@ def generator_sumary(fout: Path | None = None) -> Path:
                 pass
         fmd.writelines(
             [
-                '\n---\n\n',
-                'Copyrigth &copy; **Incolume.com.br** since 2010\n\n',
+                re.escape('\n---\n\n'),
+                re.escape('Copyrigth &copy; '
+                          '**Incolume.com.br** since 2010\n\n'),
             ],
         )
     return file

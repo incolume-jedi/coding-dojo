b4a598c27d3a6f6c059090c2e0b80a1959c0de72
Britodfbr <britodfbr@gmail.com>
2023-10-30 09:39:43 -0300

    fix: #144 PLR2004 Magic value used in comparison, consider replacing 40 with a constant variable

--- incolume/py/coding_dojo_jedi/dojo20220723/test_20220723.py
+++ incolume/py/coding_dojo_jedi/dojo20220723/test_20220723.py
@@ -1,17 +1,31 @@
 """Unittest for dojo."""
+import pytest
+
 from incolume.py.coding_dojo_jedi.dojo20220723.dojo20220723 import get_code
 
 
-def test_get_code0() -> None:
+# def test_get_code0() -> None:
+#     """Test get code."""
+#     assert get_code('(') == 40
+#
+#
+# def test_get_code1() -> None:
+#     """Test get code."""
+#     assert get_code('A') == 65
+#
+#
+# def test_get_code2() -> None:
+#     """Test get code."""
+#     assert get_code('x') == 120
+#
+@pytest.mark.parametrize(
+    'char,expected',
+    [
+        ('(', 40),
+        ('A', 65),
+        ('x', 120),
+    ],
+)
+def test_get_code(char, expected) -> None:
     """Test get code."""
-    assert get_code('(') == 40
-
-
-def test_get_code1() -> None:
-    """Test get code."""
-    assert get_code('A') == 65
-
-
-def test_get_code2() -> None:
-    """Test get code."""
-    assert get_code('x') == 120
+    assert get_code(char) == expected

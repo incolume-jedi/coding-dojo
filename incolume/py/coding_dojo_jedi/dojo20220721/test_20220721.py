e7001f8733ac126a86182465f6f740534c55c942
Britodfbr <britodfbr@gmail.com>
2023-10-31 08:02:10 -0300

    ci: #144 dojo 20220721 marcado como webtest

--- incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
+++ incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
@@ -2,9 +2,12 @@
 # !/usr/bin/env python
 from os import environ
 
+import pytest
+
 from incolume.py.coding_dojo_jedi.dojo20220721.dojo20220721 import saudacao
 
 
+@pytest.mark.webtest()
 def test_saudacao(capsys) -> None:
     """Test saudacao."""
     timeout = float(environ.get('TIMEOUT', 9))

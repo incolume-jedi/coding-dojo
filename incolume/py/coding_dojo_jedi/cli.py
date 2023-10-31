3fe4b22bf1bf41eda3acd4ad18df721ed1627cad
britodfbr <britodfbr@gmail.com>
2023-10-31 18:40:19 -0300

    feat: #154 interface CLI para sumary

---
+++ incolume/py/coding_dojo_jedi/cli.py
@@ -0,0 +1,16 @@
+from incolume.py.coding_dojo_jedi.utils import generator_sumary
+import click 
+from pathlib import Path
+
+@click.command()
+@click.option("--file", "-f", default="incolume/py/coding_dojo_jedi/README.md", help="Path for sumary file.")
+def sumary(file: str = "") -> bool:
+    """Interface CLI para gerador de sumário.
+    
+    :param file full filename for sumary;
+    :return: bool: True if success
+    """
+    fout: Path = Path(file) 
+    click.echo("ok")
+    generator_sumary(fout)
+    return fout.is_file()

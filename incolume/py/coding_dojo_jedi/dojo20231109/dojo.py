8067ddf46b6fb2fd2b5c4b55057e72e940811271
Britodfbr <britodfbr@gmail.com>
2023-11-09 22:53:18 -0300

    refactor: #61 implementação com pacote urllib

--- incolume/py/coding_dojo_jedi/dojo20231109/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20231109/dojo.py
@@ -1,5 +1,7 @@
+"""Remover anchor."""
+
 import re
-from urllib.parse import urlencode, urlsplit, urljoin
+from urllib.parse import urlparse, urlunparse
 
 
 def remove_url_anchor0(url: str) -> str:
@@ -10,14 +12,13 @@ def remove_url_anchor0(url: str) -> str:
 def remove_url_anchor1(url: str) -> str:
     """Remover anchor da url."""
     try:
-        return url[:url.index('#')]
+        return url[: url.index('#')]
     except ValueError:
         return url
 
 
 def remove_url_anchor2(url: str) -> str:
     """Remover anchor da url."""
-    # regex = r'((ht|f)tp[s]?://)?\w+'
     regex = r'((ht|f)tp[s]?://)?[\w\.\/\?\-\=]+'
     return re.search(regex, url, flags=re.I).group(0)
 
@@ -33,7 +34,6 @@ def remove_url_anchor3(url: str) -> str:
 
 def remove_url_anchor(url: str) -> str:
     """Remover anchor da url."""
-    
-    # return urlencode(url)
-    return urlsplit(url)
-    
+    result = urlparse(url)._asdict()
+    result.update({'fragment': ''})
+    return urlunparse(result.values())

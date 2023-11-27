1976f009808568d571e8ce88a42eeb295c353f58
britodfbr <britodfbr@gmail.com>
2023-11-27 14:55:50 -0300

    feat: Tratativa boyer-moore

--- incolume/py/coding_dojo_jedi/dojo20231127/test_20231127.py
+++ incolume/py/coding_dojo_jedi/dojo20231127/test_20231127.py
@@ -1,6 +1,6 @@
 """Test Get Planet Name By ID"""
 import pytest
-from incolume.py.coding_dojo_jedi.dojo20231127.dojo import get_planet_name0, get_planet_name
+from incolume.py.coding_dojo_jedi.dojo20231127.dojo import get_planet_name0, get_planet_name, boyermoore
 
 
 @pytest.mark.parametrize(
@@ -33,3 +33,18 @@ def test_get_planet_name0(entrance, expected) -> None:
 def test_get_planet_name(entrance, expected) -> None:
     """Test it."""
     assert get_planet_name(entrance) == expected
+
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    [
+        (('algoritmo', 'Os algoritmos de ordenação, são algoritmos eficazes.'), {'algoritmo': [3,20]}),
+        (('THIS', "THIS IS A TEST TEXT"), {'THIS': 0}),
+        (("AABA", 'AABAACAADAABAABA'), {'AABA': [0, 9,12]}),
+        (("1234", '012342798123468791251234'), {'1234': [1, 10, 21]}),
+    ],
+)
+def test_boyermoore(entrance, expected) -> None:
+    """Test boyer moore."""
+    assert boyermoore(*entrance) == expected

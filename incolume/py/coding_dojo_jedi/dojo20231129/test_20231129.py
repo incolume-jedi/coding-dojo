bccd3038685fc3210f5d9a5025409e27ae804fff
britodfbr <britodfbr@gmail.com>
2023-12-01 10:43:04 -0300

    feat: solução para perimetro terrestre

--- incolume/py/coding_dojo_jedi/dojo20231129/test_20231129.py
+++ incolume/py/coding_dojo_jedi/dojo20231129/test_20231129.py
@@ -1,32 +1,96 @@
 """Test dojo20231129."""
 import pytest
-from incolume.py.coding_dojo_jedi.dojo20231129.dojo import landPermetercal
+from incolume.py.coding_dojo_jedi.dojo20231129 import dojo
+
+tests = [
+    (
+        [
+            'OX',
+            'XO',
+        ],
+        'Total land perimeter: 8',
+    ),
+    (
+        [
+            'XOOXX',
+            'XOOXO',
+            'OOXXO',
+            'XXOXO',
+            'OXOXO',
+        ],
+        'Total land perimeter: 30',
+    ),
+    (
+        [
+            'XXXXX',
+            'XXXXX',
+            'XXXXX',
+            'XXXXX',
+            'XXXXX',
+        ],
+        'Total land perimeter: 20',
+    ),
+    (
+        [
+            'XOOXO',
+            'XOOXO',
+            'OOOXO',
+            'XXOXO',
+            'OXOOO',
+        ],
+        'Total land perimeter: 24',
+    ),
+    (
+        [
+            'XOOO',
+            'XOXO',
+            'XOXO',
+            'OOXX',
+            'OOOO',
+        ],
+        'Total land perimeter: 18',
+    ),
+]
 
 @pytest.mark.parametrize(
     'entrance expected'.split(),
-    [
-        (
-            [
-                'XOOXO',
-                'XOOXO',
-                'OOOXO',
-                'XXOXO',
-                'OXOOO',
-            ],
-            24,
-        ),
-        (
-            [
-                'XOOO',
-                'XOXO',
-                'XOXO',
-                'OOXX',
-                'OOOO',
-            ],
-            18,
-        ),
-    ],
+    tests,
 )
-def test_perimetro_terrestre(entrance, expected) -> None:
+def test_perimetro_terrestre_0(entrance, expected) -> None:
     """Test it."""
-    assert landPermetercal(entrance) == expected
+    assert dojo.land_perimeter_0(entrance) == expected
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    tests,
+)
+def test_perimetro_terrestre_1(entrance, expected) -> None:
+    """Test it."""
+    assert dojo.land_perimeter_1(entrance) == expected
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    tests,
+)
+def test_perimetro_terrestre_2(entrance, expected) -> None:
+    """Test it."""
+    assert dojo.land_perimeter_2(entrance) == expected
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    tests,
+)
+def test_perimetro_terrestre_3(entrance, expected) -> None:
+    """Test it."""
+    assert dojo.land_perimeter_3(entrance) == expected
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    tests,
+)
+def test_landPermetecal(entrance, expected) -> None:
+    """Test it."""
+    assert dojo.land_permetercal(entrance) == expected

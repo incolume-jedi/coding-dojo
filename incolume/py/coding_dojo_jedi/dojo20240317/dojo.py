736a150bea829bfb61a6f8698354023124829054
Britodfbr <britodfbr@gmail.com>
2024-03-17 18:44:46 -0300

    feat: #57 solução para `eu sou o perímetro`

--- incolume/py/coding_dojo_jedi/dojo20240317/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20240317/dojo.py
@@ -1,7 +1,27 @@
-__author__ = '@britodfbr'  # pragma: no cover
+"""Dojo realizado em 17 Mar 2024."""
+
+__author__ = "@britodfbr"  # pragma: no cover
+
 
 def perimeter(matriz: list[list, ...]) -> int:
     """Perimeter for matrix."""
+    limits = 2, 100
+
     if len(matriz) == 0:
         return 0
-    return None
+
+    n = len(matriz)
+    m = len(matriz[0])
+
+    if n < limits[0]:
+        raise ValueError
+
+    if m > limits[-1]:
+        raise ValueError
+
+    perim = sum(matriz[0][1:-1]) + sum(matriz[-1][1:-1])
+
+    for i in range(n):
+        perim += matriz[i][0] + matriz[i][-1]
+
+    return perim
--- incolume/py/coding_dojo_jedi/dojo20240317/test_20240317.py
+++ incolume/py/coding_dojo_jedi/dojo20240317/test_20240317.py
@@ -1,17 +1,20 @@
+"""Test module for dojo20240317."""
 from incolume.py.coding_dojo_jedi.dojo20240317.dojo import perimeter
 import pytest
 
 
-__author__ = '@britodfbr'  # pragma: no cover
+__author__ = "@britodfbr"  # pragma: no cover
 
 
 @pytest.mark.parametrize(
-    'entrance expected'.split(),
+    "entrance expected".split(),
     [
         ([], 0),
-        ([[1, 2, 3],[4, 5, 6], [7, 8, 9]], 10),
+        ([[1, 2], [3, 4]], 10),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 40),
+        ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]], 102),
     ],
 )
-def test_perimeter(entrance, expected):
-    """"""
-    assert perimeter( entrance ) == expected
+def test_perimeter(entrance, expected) -> None:
+    """Test it."""
+    assert perimeter(entrance) == expected

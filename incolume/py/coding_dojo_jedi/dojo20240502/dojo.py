729d947d1e729359c13fb0392a880258435ffd87
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-05-02 11:28:36 -0300

    feat: Solução no paradigma funcional

--- incolume/py/coding_dojo_jedi/dojo20240502/dojo.py
+++ incolume/py/coding_dojo_jedi/dojo20240502/dojo.py
@@ -46,3 +46,48 @@ def check_cpf(cpf: int | str) -> bool:
 
     return novo == antigo
 
+
+def verify_cpf(cpf: int | str) -> bool:
+    """Verificar se CPF é válido.
+
+    Parâmetros:
+        cpf (str|int): CPF a ser validado
+
+    Retorno:
+        bool:
+            - Falso, quando o CPF não possuir o formato 999.999.999-99;
+            - Falso, quando o CPF não possuir 11 caracteres numéricos;
+            - Falso, quando os dígitos verificadores forem inválidos;
+            - Verdadeiro, caso contrário.
+
+    Exemplos:
+
+    >>> verify_cpf('529.982.247-25')
+    True
+    >>> verify_cpf('52998224725')
+    False
+    >>> verify_cpf('111.111.111-11')
+    False
+    """
+    # Obtém apenas os números do CPF, ignorando pontuações
+    numbers = [int(digit) for digit in str(cpf) if digit.isdigit()]
+
+    # Verifica se o CPF possui 11 números ou se todos são iguais:
+    if not cpf or len(numbers) < CPF_DIGITS or len(set(numbers)) == 1:
+        return False
+
+    # Validação do primeiro dígito verificador:
+    sum_of_products = sum(
+        a * b for a, b in zip(numbers[0:9], range(10, 1, -1), strict=False)
+    )
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[9] != expected_digit:
+        return False
+
+    # Validação do segundo dígito verificador:
+    sum_of_products = sum(
+        a * b for a, b in zip(numbers[0:10], range(11, 1, -1), strict=False)
+    )
+    expected_digit = (sum_of_products * 10 % 11) % 10
+
+    return numbers[10] == expected_digit
--- incolume/py/coding_dojo_jedi/dojo20240502/test_20240502.py
+++ incolume/py/coding_dojo_jedi/dojo20240502/test_20240502.py
@@ -27,3 +27,22 @@ class TestCheckCPF:
         """Test it."""
         assert check_cpf(entrance) == expected
 
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            ('000.000.001-91', True),
+            ('000.000.002-72', True),
+            ('000.000.003-53', True),
+            ('000.000.000-00', False),
+            ('123.456.789-12', False),
+            ('529.982.247-25', True),
+            ('777.777.777-77', False),
+            (49691275228, True),
+            (56681418104, True),
+            ('777.777.777', False),
+            ('', False),
+        ],
+    )
+    def test_verify_cpf(self, entrance, expected):
+        """Test it."""
+        assert verify_cpf(entrance) == expected

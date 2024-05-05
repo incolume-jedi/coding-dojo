7c4a07d384e1ab021c1945fa2956f3cf618e4408
Britodfbr <britodfbr@gmail.com>
2024-05-05 13:17:15 -0300

    feat: estrutura para dojo 20240503 gerar CPF

---
+++ incolume/py/coding_dojo_jedi/dojo20240503/__init__.py
@@ -0,0 +1,14 @@
+"""Module."""
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import random
+import secrets
+from typing import Final
+
+__author__ = '@britodfbr'  # pragma: no cover
+CPF_DIGITS: Final[int] = 11
+
+
---
+++ incolume/py/coding_dojo_jedi/dojo20240503/test_20240503.py
@@ -0,0 +1,15 @@
+"""Module."""
+
+from typing import NoReturn
+from unittest import mock
+
+import pytest
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import random, gen_cpf0, gen_cpf1
+
+__author__ = '@britodfbr'  # pragma: no cover
+
+

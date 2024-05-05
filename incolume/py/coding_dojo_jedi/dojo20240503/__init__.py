457684131051b7b8d1b594963254fa7fef5b8b31
Britodfbr <britodfbr@gmail.com>
2024-05-05 13:23:29 -0300

    feat: implementação imperativa com código pseudo aleatório (random)

--- incolume/py/coding_dojo_jedi/dojo20240503/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240503/__init__.py
@@ -12,3 +12,15 @@ __author__ = '@britodfbr'  # pragma: no cover
 CPF_DIGITS: Final[int] = 11
 
 
+def gen_cpf0() -> str:
+    """Gerador de CPF aleatórios."""
+    prefix = [random.randint(0, 9) for _ in range(9)]  # noqa: S311
+
+    while len(prefix) < CPF_DIGITS:
+        resto = (
+            sum(v * (len(prefix) + 1 - i) for i, v in enumerate(prefix)) % 11
+        )
+        digito_verificador = 0 if resto <= 1 else 11 - resto
+        prefix.append(digito_verificador)
+    return ''.join(str(n) for n in prefix)
+
--- incolume/py/coding_dojo_jedi/dojo20240503/test_20240503.py
+++ incolume/py/coding_dojo_jedi/dojo20240503/test_20240503.py
@@ -13,3 +13,18 @@ from . import random, gen_cpf0, gen_cpf1
 __author__ = '@britodfbr'  # pragma: no cover
 
 
+class CheckDojoGenCPF:
+    """class test."""
+
+    @pytest.mark.parametrize(
+        'seed expected'.split(),
+        [
+            (0, '66048764707'),
+            (7, '52601815906'),
+            (11, '78778932807'),
+        ],
+    )
+    def test_gen_pdf0(self, seed, expected) -> NoReturn:
+        """Test it."""
+        random.seed(seed)
+        assert gen_cpf0() == expected

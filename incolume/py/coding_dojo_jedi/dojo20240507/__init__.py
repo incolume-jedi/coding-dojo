bdcd0d0c1e907dd5a2b8e48e4d195ee268250658
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-05-07 08:31:51 -0300

    feat: Solução para dojo 20240507 - Todos Números felizes menores que 100

--- incolume/py/coding_dojo_jedi/dojo20240507/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240507/__init__.py
@@ -1,1 +1,21 @@
 """Module dojo."""
+
+from functools import lru_cache
+
+
+@lru_cache
+def is_feliz(num: int) -> bool:
+    """Verifica se numero é feliz."""
+    num = [pow(int(n), 2) for n in str(num)]
+    if (snum := sum(num)) == 1:
+        return True
+
+    try:
+        return is_feliz(snum)
+    except RecursionError:
+        return False
+
+
+def num_feliz(num: int) -> list[int]:
+    """Retorna numeros felizes menores que num."""
+    return [n for n in range(num + 1) if is_feliz(n)]

778754e1956df736649487509c376c97299fd4a9
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-05-07 09:38:03 -0300

    feat: Solução para dojo `Quais são os primeiros 25 números felizes?`
    close #70

---
+++ incolume/py/coding_dojo_jedi/dojo20240507/README.md
@@ -0,0 +1,14 @@
+# Coding Dojo
+**Guilda JEDI Incolume - Grupo Python Incolume**
+
+---
+
+Neste dojo resolvemos 2 problemas:
+
+- [Todos Números felizes menores que 100](./problema1.md)
+- [Quais são os primeiros 25 números felizes?](./problema2.md)
+
+
+---
+
+Copyright © incolume.com.br since 2010
--- incolume/py/coding_dojo_jedi/dojo20240507/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240507/__init__.py
@@ -1,6 +1,7 @@
 """Module dojo."""
 
 from functools import lru_cache
+from itertools import count
 
 
 @lru_cache
@@ -19,3 +20,14 @@ def is_feliz(num: int) -> bool:
 def num_feliz(num: int) -> list[int]:
     """Retorna numeros felizes menores que num."""
     return [n for n in range(num + 1) if is_feliz(n)]
+
+
+@lru_cache
+def list_feliz(quantia: int) -> list[int]:
+    """Retorna uma lista com a quantia desejada de numeros felizes."""
+    result = []
+    sequence = count(1, 1)
+    while len(result) < quantia:
+        if is_feliz(n := next(sequence)):
+            result.append(n)
+    return result

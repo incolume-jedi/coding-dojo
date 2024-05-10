02d5f4aefdae814101f3328ed9a7e8078a80f70d
Britodfbr <britodfbr@gmail.com>
2024-05-10 08:51:44 -0300

    feat: Solução dojo-posição-das-letras
    close #76

--- incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
@@ -2,6 +2,9 @@
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from collections import defaultdict
+
 __author__ = '@britodfbr'  # pragma: no cover
 
 
@@ -9,3 +12,11 @@ def is_narcisist(num: int) -> bool:
     """Check if number is narcisist number."""
     result = sum(pow(int(n), len(str(num))) for n in str(num) if n.isdigit())
     return num == result
+
+
+def char_position(text: str) -> dict[str, list[int]]:
+    """Mapeia os caracteres de uma string."""
+    result = defaultdict(list)
+    for i, char in enumerate(text):
+        result[char].append(i)
+    return result

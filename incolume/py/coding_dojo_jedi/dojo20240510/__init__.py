8baf9cf99591d1a2d38e33d3a4dbe500f454e166
Britodfbr <britodfbr@gmail.com>
2024-05-10 08:11:25 -0300

    feat: Solução para Número Narcisista
    close #75

--- incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
@@ -3,3 +3,9 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'  # pragma: no cover
+
+
+def is_narcisist(num: int) -> bool:
+    """Check if number is narcisist number."""
+    result = sum(pow(int(n), len(str(num))) for n in str(num) if n.isdigit())
+    return num == result

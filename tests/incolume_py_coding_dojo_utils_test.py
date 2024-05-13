e198c045c64c6e1fad9e199baf8d13b201f092ff
Britodfbr <britodfbr@gmail.com>
2024-05-13 08:53:07 -0300

    tests[refactor]: implementado class para caso de testes no modulo utils

--- tests/incolume_py_coding_dojo_utils_test.py
+++ tests/incolume_py_coding_dojo_utils_test.py
@@ -6,7 +6,11 @@ from pathlib import Path
 
 import pytest
 
-from incolume.py.coding_dojo_jedi.utils import filesmd, generator_sumary
+from incolume.py.coding_dojo_jedi.utils import (
+    MD_DIR,
+    filesmd,
+    generator_sumary,
+)
 
 
 @pytest.fixture()
@@ -25,72 +29,90 @@ def count_links(arq_entrada: Path) -> int:
     return contagem
 
 
-@pytest.mark.skipif(
-    sys.version_info < (3, 10),
-    reason='requires python3.10 or higher',
-)
-@pytest.mark.skipif(
-    sys.platform.startswith('win'),
-    reason='Not available on windows.',
-)
-def test_quantia(filemd) -> None:  # pylint: disable=redefined-outer-name
-    """Testar se a quantidade de links e dojos são iguais."""
-    arq = next(Path(__file__).absolute().parents[1].rglob('coding_dojo_jedi'))
-    c_links = count_links(generator_sumary(filemd))
-    c_dirs = len(filesmd())
-    logging.debug('%s %s %s', arq, c_dirs, c_links)
-    assert c_links == c_dirs
+class TestUtilsModule:
+    """Case test utils."""
 
+    def test_md_dir_type(self):
+        """Check directory type."""
+        assert isinstance(MD_DIR, Path)
 
-@pytest.mark.skipif(
-    sys.version_info < (3, 10),
-    reason='requires python3.10 or higher',
-)
-@pytest.mark.parametrize(
-    'entrance',
-    [
-        pytest.param(
-            r'# Coding Dojo',
-            marks=pytest.mark.skipif(
-                sys.platform.startswith('win'),
-                reason='Does not run on windows.',
+    def test_md_dir_value(self):
+        """Check directory value."""
+        assert MD_DIR.parts[-4:] == (
+            'coding-dojo-jedi',
+            'incolume',
+            'py',
+            'coding_dojo_jedi',
+        )
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 10),
+        reason='requires python3.10 or higher',
+    )
+    @pytest.mark.skipif(
+        sys.platform.startswith('win'),
+        reason='Not available on windows.',
+    )
+    def test_quantia(self, filemd) -> None:  # pylint: disable=redefined-outer-name
+        """Testar se a quantidade de links e dojos são iguais."""
+        arq = next(
+            Path(__file__).absolute().parents[1].rglob('coding_dojo_jedi'),
+        )
+        c_links = count_links(generator_sumary(filemd))
+        c_dirs = len(filesmd())
+        logging.debug('%s %s %s', arq, c_dirs, c_links)
+        assert c_links == c_dirs
+
+    @pytest.mark.skipif(
+        sys.version_info < (3, 10),
+        reason='requires python3.10 or higher',
+    )
+    @pytest.mark.parametrize(
+        'entrance',
+        [
+            pytest.param(
+                r'# Coding Dojo',
+                marks=pytest.mark.skipif(
+                    sys.platform.startswith('win'),
+                    reason='Does not run on windows.',
+                ),
             ),
-        ),
-        pytest.param(
-            '**Guilda JEDI Incolume - Grupo Python Incolume**',
-            marks=pytest.mark.skipif(
-                sys.platform.startswith('win'),
-                reason='Does not run on windows.',
+            pytest.param(
+                '**Guilda JEDI Incolume - Grupo Python Incolume**',
+                marks=pytest.mark.skipif(
+                    sys.platform.startswith('win'),
+                    reason='Does not run on windows.',
+                ),
             ),
-        ),
-        pytest.param(
-            '- [Seja membro da Guilda JEDI Incolume]'
-            '(https://discord.gg/eBNamXVtBW)',
-            marks=pytest.mark.skipif(
-                sys.platform.startswith('win'),
-                reason='Does not run on windows.',
+            pytest.param(
+                '- [Seja membro da Guilda JEDI Incolume]'
+                '(https://discord.gg/eBNamXVtBW)',
+                marks=pytest.mark.skipif(
+                    sys.platform.startswith('win'),
+                    reason='Does not run on windows.',
+                ),
             ),
-        ),
-        pytest.param(
-            '## Sumário dos dojos',
-            marks=pytest.mark.skipif(
-                sys.platform.startswith('win'),
-                reason='Does not run on windows.',
+            pytest.param(
+                '## Sumário dos dojos',
+                marks=pytest.mark.skipif(
+                    sys.platform.startswith('win'),
+                    reason='Does not run on windows.',
+                ),
             ),
-        ),
-        pytest.param(
-            '&copy; **Incolume.com.br**',
-            marks=pytest.mark.skipif(
-                sys.platform.startswith('win'),
-                reason='Does not run on windows.',
+            pytest.param(
+                '&copy; **Incolume.com.br**',
+                marks=pytest.mark.skipif(
+                    sys.platform.startswith('win'),
+                    reason='Does not run on windows.',
+                ),
             ),
-        ),
-    ],
-)
-def test_content_sumary(
-    filemd,  # pylint: disable=redefined-outer-name
-    entrance,
-) -> None:
-    """Teste para escopo do sumário."""
-    file = generator_sumary(filemd)
-    assert entrance in file.read_text()
+        ],
+    )
+    def test_content_sumary(
+        self,
+        filemd,  # pylint: disable=redefined-outer-name
+        entrance,
+    ) -> None:
+        """Teste para escopo do sumário."""
+        file = generator_sumary(filemd)
+        assert entrance in file.read_text()

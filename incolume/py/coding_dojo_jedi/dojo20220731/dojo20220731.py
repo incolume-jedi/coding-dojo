fe21ebd34f6691c42cc803504526496751728dcf
Brito <britodfbr@users.noreply.github.com>
2024-05-21 07:14:08 -0300

    feat: Nova implementação para dojo20220731 tabela ASCII.

--- incolume/py/coding_dojo_jedi/dojo20220731/dojo20220731.py
+++ incolume/py/coding_dojo_jedi/dojo20220731/dojo20220731.py
@@ -1,10 +1,15 @@
 """Dojo 2022-07-31."""
 
 
-def show_table_ascii() -> list:
+def show_table_ascii0() -> list:
     """Exibe a tabela ascii."""
     lista = []
     for code in range(256):
         letra = chr(code)
         lista.append((code, letra))
     return lista
+
+
+def show_table_ascii() -> list:
+    """Exibe a tabela ascii."""
+    return [(code, chr(code)) for code in range(256)]

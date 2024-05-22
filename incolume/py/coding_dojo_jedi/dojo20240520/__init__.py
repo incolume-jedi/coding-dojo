92290f65a28d4fc14705b56e15a179855fcf52af
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-05-22 09:51:14 -0300

    feat: Outra implementação para Rot13 e respectivos testes
    close #130

--- incolume/py/coding_dojo_jedi/dojo20240520/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240520/__init__.py
@@ -11,10 +11,18 @@ def rot13a(texto: str) -> str:
     return ''.join([d.get(c, c) for c in texto])
 
 
-def rot13(texto: str) -> str:
+def rot13b(texto: str) -> str:
     """Rot13."""
     encode = str.maketrans(
         'ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
         'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm',
     )
+    return texto.translate(encode)
+
+
+def rot13(texto: str) -> str:
+    """Rot13."""
+    encode = {
+        (i + c): ((i + 13) % 26 + c) for i in range(26) for c in (65, 97)
+    }
     return texto.translate(encode)
--- incolume/py/coding_dojo_jedi/dojo20240520/test_20240520.py
+++ incolume/py/coding_dojo_jedi/dojo20240520/test_20240520.py
@@ -1,22 +1,48 @@
 """Test module."""
 
 import pytest
-from . import rot13
+from . import rot13, rot13a, rot13b
+
+
+testes = [
+    ('EBG13 rknzcyr.', 'ROT13 example.'),
+    (
+        'This is my first ROT13 excercise!',
+        'Guvf vf zl svefg EBG13 rkprepvfr!',
+    ),
+    (
+        "Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.",
+        "In the elevators, the extrovert looks at the OTHER guy's shoes.",
+    ),
+    (
+        'Tudo é difícil, até fácil se tornar.',
+        'Ghqb é qvsípvy, ngé sápvy fr gbeane.',
+    ),
+    ('Znl gur sbepr or jvgu lbh', 'May the force be with you'),
+]
 
 
 @pytest.mark.parametrize(
     'entrance expected'.split(),
-    [
-        ('EBG13 rknzcyr.', 'ROT13 example.'),
-        (
-            'This is my first ROT13 excercise!',
-            'Guvf vf zl svefg EBG13 rkprepvfr!',
-        ),
-        (
-            "Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.",
-            "In the elevators, the extrovert looks at the OTHER guy's shoes.",
-        ),
-    ],
+    testes,
+)
+def test_rot13a(entrance, expected):
+    """Test."""
+    assert rot13a(entrance) == expected
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    testes,
+)
+def test_rot13b(entrance, expected):
+    """Test."""
+    assert rot13b(entrance) == expected
+
+
+@pytest.mark.parametrize(
+    'entrance expected'.split(),
+    testes,
 )
 def test_rot13(entrance, expected):
     """Test."""

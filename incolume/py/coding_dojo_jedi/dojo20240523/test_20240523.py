211b40e70b37f8288ca56d80a16fb867b487cf0d
Britodfbr <britodfbr@gmail.com>
2024-05-27 10:38:18 -0300

    feat: solução dojo análise de notas
    close #137

--- incolume/py/coding_dojo_jedi/dojo20240523/test_20240523.py
+++ incolume/py/coding_dojo_jedi/dojo20240523/test_20240523.py
@@ -1,3 +1,68 @@
 """Module."""
 
 __author__ = '@britodfbr'  # pragma: no cover
+
+from unittest import mock
+
+from . import analise_nota
+
+
+class Testcase:
+    """Test case."""
+
+    def test_quantidade(self, capsys):
+        """Quantidade."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'quantidade: 2;' in out
+
+    def test_list(self, capsys):
+        """Quantidade."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Notas: 7 5;' in out
+
+    def test_rev(self, capsys):
+        """Quantidade."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Reverso: 5 7;' in out
+
+    def test_soma(self, capsys):
+        """Quantidade."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Soma: 12;' in out
+
+    def test_media(self, capsys):
+        """Média."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Média: 6.0;' in out
+
+    def test_acima_media(self, capsys):
+        """Média acima."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Acima da média: 7;' in out
+
+    def test_abaixo_sete(self, capsys):
+        """Média acima."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+        assert 'Abaixo de 7: 5;' in out
+
+    def test_msg_out(self, capsys):
+        """Test dojo."""
+        with mock.patch('builtins.input', side_effect=[7, 5, -1]):
+            analise_nota()
+            out, _ = capsys.readouterr()
+
+            assert 'Bye bye.' in out

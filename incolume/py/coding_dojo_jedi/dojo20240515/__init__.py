af055637ae5e8d15d27a0450e55234ce184b9318
Britodfbr <britodfbr@gmail.com>
2024-05-31 15:54:57 -0300

    feat: Implementado ambiente mock para testes em API deepl para tradução de idiomas.
    close #226

--- incolume/py/coding_dojo_jedi/dojo20240515/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240515/__init__.py
@@ -1,6 +1,7 @@
 """Module dojo."""
 
 import logging
+import os
 from http import HTTPStatus
 from os import environ
 from pathlib import Path
@@ -8,9 +9,14 @@ from pathlib import Path
 import deepl
 import httpx
 from dotenv import load_dotenv
+from icecream import ic
 
 load_dotenv(Path(__file__).parent / '.env')
 url_api = 'https://api-free.deepl.com/v2/translate'
+
+ic.disable()
+if os.getenv('DEBUG_MODE'):
+    ic.enable()
 
 
 def translate_deepl_api(text: str, lang: str = 'EN', url: str = '') -> dict:
@@ -25,16 +31,20 @@ def translate_deepl_api(text: str, lang: str = 'EN', url: str = '') -> dict:
         'target_lang': lang,
     }
     resp = httpx.post(url, headers=header, json=data)
+    ic(resp)
+    ic(resp.status_code)
     if resp.status_code == HTTPStatus.OK:
+        ic(resp.json())
         return resp.json()
     return {}
 
 
-def translate_deepl(text: str, lang: str = 'EN-US') -> dict:
+def translate_deepl(text: str, lang: str = 'EN-US') -> str:
     """Translate with deepl API."""
     translator = deepl.Translator(environ.get('DEEPL_API_KEY'))
 
     result = translator.translate_text(text=text, target_lang=lang)
+    ic(result)
     return result.text
 
 
--- incolume/py/coding_dojo_jedi/dojo20240515/test_20240515.py
+++ incolume/py/coding_dojo_jedi/dojo20240515/test_20240515.py
@@ -2,7 +2,7 @@
 
 from unittest import mock
 from http import HTTPStatus
-from incolume.py.coding_dojo_jedi.dojo20240515 import (
+from . import (
     translate_deepl,
     translate_deepl_api,
 )
@@ -21,37 +21,17 @@ class CheckDeepl:
         ('FR', 'Bonsoir à tous'),
     ]
 
-    @pytest.mark.skip()
+    @pytest.mark.skip(reason='replaced for test_translate_deepl_api_mock')
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         tests0,
     )
-    def test_tranlate_deepl_api(self, entrance, expected):
+    def test_translate_deepl_api(self, entrance, expected):
         """Test tranlate deepl."""
         result = translate_deepl_api('Boa Noite', entrance)
         assert result['translations'][0]['text'] == expected
 
-    @pytest.mark.parametrize(
-        'entrance expected'.split(),
-        tests0,
-    )
-    def test_tranlate_deepl_api_mock(self, entrance, expected):
-        """Test tranlate deepl."""
-        with mock.patch('httpx.post', return_value=mock.MagicMock()) as m_resp:
-            m_resp.status_code = HTTPStatus.OK
-            m_resp.json.return_value = {
-                'translations': [
-                    {
-                        'detected_source_language': 'PT',
-                        'text': expected,
-                    },
-                ],
-            }
-            result = translate_deepl_api(text='Boa Noite', lang=entrance)
-            assert result == ''
-            assert result['translations'][0]['text'] == expected
-
-    @pytest.mark.skip()
+    @pytest.mark.skip(reason='replaced for test_translate_deepl_mock')
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         tests0,
@@ -59,3 +39,34 @@ class CheckDeepl:
     def test_tranlate_deepl(self, entrance, expected):
         """Test tranlate deepl."""
         assert translate_deepl('Boa Noite', entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        tests0,
+    )
+    def test_translate_deepl_api_mock(self, entrance, expected):
+        """Test tranlate deepl."""
+        obj_resp = mock.MagicMock()
+        obj_resp.status_code = HTTPStatus.OK
+        obj_resp.json.return_value = {
+            'translations': [
+                {
+                    'detected_source_language': 'PT',
+                    'text': expected,
+                },
+            ],
+        }
+        with mock.patch('httpx.post', side_effect=[obj_resp]):
+            result = translate_deepl_api(text='Boa Noite', lang=entrance)
+            assert result['translations'][0]['text'] == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        tests0,
+    )
+    def test_tranlate_deepl_mock(self, entrance, expected):
+        """Test tranlate deepl."""
+        oresp = mock.MagicMock()
+        oresp.translate_text.return_value.text = expected
+        with mock.patch('deepl.Translator', return_value=oresp):
+            assert translate_deepl('Boa Noite', entrance) == expected

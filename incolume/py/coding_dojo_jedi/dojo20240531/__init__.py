e5acc4328ad4c28bd148c6ac9699d1c14d688393
Britodfbr <britodfbr@gmail.com>
2024-05-31 14:19:33 -0300

    feat: solução para dojo último dígito de 2²⁷
    close #160

--- incolume/py/coding_dojo_jedi/dojo20240531/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240531/__init__.py
@@ -3,3 +3,25 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'  # pragma: no cover
+
+import logging
+
+
+def last_dig0(base: int, expoente: int) -> int:
+    """Retorna o ultimo digito de uma potência."""
+    result = base**expoente
+    return result % 10
+
+
+def last_dig1(base: int, expoente: int) -> int:
+    """Retorna o ultimo digito de uma potência."""
+    logging.debug(base, expoente)
+    result = [int(x) for x in '2486']
+    return result[expoente % len(result) - 1]
+
+
+def last_dig(base: int, expoente: int) -> int:
+    """Retorna o ultimo digito de uma potência."""
+    result = [int(x) for x in {(base**i) % 10 for i in range(1, 5)}]
+    result.sort()
+    return result[expoente % len(result)]

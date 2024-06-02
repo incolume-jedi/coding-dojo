601ab4c6bf32d1b54a6ca63248c45ea3dbb37a4c
Britodfbr <britodfbr@gmail.com>
2024-06-02 13:59:27 -0300

    feat: implementação de raspagem com BS4 e httpx

--- incolume/py/coding_dojo_jedi/dojo20240602/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240602/__init__.py
@@ -2,4 +2,74 @@
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+from collections import OrderedDict
+from inspect import stack
+from pathlib import Path
+
+import httpx
+from bs4 import BeautifulSoup
+from icecream import ic
+
 __author__ = '@britodfbr'  # pragma: no cover
+
+
+ic.disable()
+if os.getenv('DEBUG_MODE'):
+    ic.enable()
+
+
+class CBF:
+    """Campeonato Brasileiro de Futebol."""
+
+    def __init__(self, url_or_path: str) -> None:
+        """Init."""
+        self.url_or_path: str = ic(url_or_path)
+        self.content: OrderedDict = OrderedDict()
+        self.scrap()
+
+    def scrap(self) -> None:
+        """Scrap content."""
+        try:
+            response = httpx.get(self.url_or_path)
+            soup = BeautifulSoup(response.content, 'html5lib')
+        except httpx.UnsupportedProtocol:
+            with Path(self.url_or_path).open('rb') as f:
+                soup = BeautifulSoup(f.read(), 'html5lib')
+        result = soup.table
+
+        d = OrderedDict()
+        for th, td in zip(
+            soup.select('th')[::2],
+            soup.select('td'),
+        ):
+            d[th.text.strip()] = td.text.strip().splitlines()
+
+        # title = [th.text for th in result.select('th span:nth-of-type(1)')]
+        # content = [td.text for td in result.select('td')]
+        # content = len(result.select('td'))
+        # title = [th.text for th in result.select('th div:nth-of-type(1)')]
+        # content = title
+        content = d
+        self.content = content
+
+    def sort_by_name(self, *, reverse: bool = False) -> list:
+        """Sort by name."""
+
+    def sort_by_point(self, *, reverse: bool = False) -> list:
+        """Sort by point."""
+
+    def classify_libertadores(self):
+        """Classificado para fase de grupo da libertadores."""
+
+    def qualify_libertadores(self):
+        """Selecionados para Qualificatórias da libertadores."""
+
+    def select_sulamericana(self):
+        """Selecionados para fase de grupos da copa sulamericana."""
+
+    def rebaixados(self):
+        """Clubes rebaixados."""
+
+    def classify(self, clube: str) -> int:
+        """Classificação por pontos no Campeonato brasileiro de futebol."""
--- incolume/py/coding_dojo_jedi/dojo20240602/test_20240602.py
+++ incolume/py/coding_dojo_jedi/dojo20240602/test_20240602.py
@@ -1,3 +1,34 @@
 """Module."""
 
+from pathlib import Path
+
+import pytest
+
+from . import CBF
+
 __author__ = '@britodfbr'  # pragma: no cover
+
+
+class TestCaseCBF:
+    """Test case."""
+
+    url = 'http://localhost:8000'
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                'http://localhost:8000',
+                '',
+                marks=pytest.mark.webtest,
+            ),
+            pytest.param(
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                '',
+            ),
+        ],
+    )
+    def test_content(self, entrance, expected):
+        """Test content."""
+        obj = CBF(entrance)
+        assert obj.content == expected

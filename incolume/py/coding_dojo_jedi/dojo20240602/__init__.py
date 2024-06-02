f2d0ebc982403c22ec45b47245766ab3282761f0
Britodfbr <britodfbr@gmail.com>
2024-06-02 14:15:07 -0300

    feat: implementação com pandas para raspagem de dados tabulares
    close #170

--- incolume/py/coding_dojo_jedi/dojo20240602/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240602/__init__.py
@@ -4,12 +4,14 @@
 # -*- coding: utf-8 -*-
 import os
 from collections import OrderedDict
-from inspect import stack
+from itertools import count
 from pathlib import Path
 
 import httpx
+import pandas as pd
 from bs4 import BeautifulSoup
 from icecream import ic
+from unidecode import unidecode
 
 __author__ = '@britodfbr'  # pragma: no cover
 
@@ -26,9 +28,9 @@ class CBF:
         """Init."""
         self.url_or_path: str = ic(url_or_path)
         self.content: OrderedDict = OrderedDict()
-        self.scrap()
+        self._scrap()
 
-    def scrap(self) -> None:
+    def __scrap(self) -> None:
         """Scrap content."""
         try:
             response = httpx.get(self.url_or_path)
@@ -42,34 +44,90 @@ class CBF:
         for th, td in zip(
             soup.select('th')[::2],
             soup.select('td'),
+            strict=False,
         ):
             d[th.text.strip()] = td.text.strip().splitlines()
 
-        # title = [th.text for th in result.select('th span:nth-of-type(1)')]
-        # content = [td.text for td in result.select('td')]
-        # content = len(result.select('td'))
-        # title = [th.text for th in result.select('th div:nth-of-type(1)')]
-        # content = title
+        title = [th.text for th in result.select('th span:nth-of-type(1)')]
+        content = [td.text for td in result.select('td')]
+        content = len(result.select('td'))
+        title = [th.text for th in result.select('th div:nth-of-type(1)')]
+        content = title
         content = d
         self.content = content
 
+    def _scrap(self):
+        """Scrap content."""
+        content = pd.read_html(self.url_or_path, encoding='utf-8')
+        df: pd.DataFrame = ic(content[0])
+        df.drop(df.columns[[0, 11, 12]], axis=1, inplace=True)  # noqa: PD002
+        df.columns = df.columns.str.upper()
+        df.columns = [
+            'RANK',
+            'CLUBE',
+            'PONTOS',
+            'PARTIDAS JOGADAS',
+            'VITÓRIAS',
+            'EMPATES',
+            'DERROTAS',
+            'GOLS MARCADOS',
+            'GOLS CONTRA',
+            'SALDO DE GOLS',
+        ]
+
+        df['STATUS'] = df.RANK.apply(
+            lambda x: ''.join(n for n in x if not n.isdigit()),
+        )
+        c = count(1)
+        df.RANK = df.RANK.map(lambda x: next(c))  # noqa: ARG005
+        ic(df.head())
+        self.content = df.to_dict()
+
     def sort_by_name(self, *, reverse: bool = False) -> list:
         """Sort by name."""
+        return sorted((self.content.get('CLUBE').values()), reverse=reverse)
 
     def sort_by_point(self, *, reverse: bool = False) -> list:
         """Sort by point."""
+        rank = sorted(
+            (
+                (k, v, m)
+                for k, v, m in zip(
+                    self.content.get('PONTOS').values(),
+                    self.content.get('SALDO DE GOLS').values(),
+                    self.content.get('CLUBE').values(),
+                    strict=False,
+                )
+            ),
+            reverse=reverse,
+        )
+        return [clube[2] for clube in rank]
 
     def classify_libertadores(self):
         """Classificado para fase de grupo da libertadores."""
+        return self.classify()[:4]
 
     def qualify_libertadores(self):
         """Selecionados para Qualificatórias da libertadores."""
+        return self.classify()[4:6]
 
     def select_sulamericana(self):
         """Selecionados para fase de grupos da copa sulamericana."""
+        return self.classify()[6:12]
 
     def rebaixados(self):
         """Clubes rebaixados."""
+        return self.classify()[-4:]
 
-    def classify(self, clube: str) -> int:
+    def classify(self) -> list:
         """Classificação por pontos no Campeonato brasileiro de futebol."""
+        return self.sort_by_point(reverse=True)
+
+    def clube(self, clube_name: str) -> int:
+        """Classificação por pontos no Campeonato brasileiro de futebol."""
+        clubes = {
+            unidecode(v.casefold()): k
+            for k, v in self.content.get('CLUBE').items()
+        }
+        index = clubes.get(unidecode(clube_name.casefold()))
+        return self.content.get('PONTOS').get(index)
--- incolume/py/coding_dojo_jedi/dojo20240602/test_20240602.py
+++ incolume/py/coding_dojo_jedi/dojo20240602/test_20240602.py
@@ -1,6 +1,7 @@
 """Module."""
 
 from pathlib import Path
+from typing import ClassVar
 
 import pytest
 
@@ -12,23 +13,237 @@ __author__ = '@britodfbr'  # pragma: no cover
 class TestCaseCBF:
     """Test case."""
 
-    url = 'http://localhost:8000'
+    url: ClassVar = 'http://localhost:8000'
+    test_case_0: ClassVar = [
+        pytest.param(
+            'http://localhost:8000',
+            '',
+            marks=pytest.mark.webtest,
+        ),
+        pytest.param(
+            Path(__file__).parent.joinpath('index.html').as_posix(),
+            '',
+        ),
+    ]
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
-        [
-            pytest.param(
-                'http://localhost:8000',
-                '',
-                marks=pytest.mark.webtest,
-            ),
-            pytest.param(
-                Path(__file__).parent.joinpath('index.html').as_posix(),
-                '',
-            ),
-        ],
+        test_case_0,
     )
     def test_content(self, entrance, expected):
         """Test content."""
+        expected = True
         obj = CBF(entrance)
-        assert obj.content == expected
+        assert isinstance(obj.content, dict) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_sort_by_name(self, entrance, expected):
+        """Test sort."""
+        obj = CBF(entrance)
+        expected = [
+            'América-MG',
+            'Athletico-PR',
+            'Atlético-GO',
+            'Atlético-MG',
+            'Bahia',
+            'Bragantino',
+            'Ceará SC',
+            'Chapecoense',
+            'Corinthians',
+            'Cuiabá',
+            'Flamengo',
+            'Fluminense',
+            'Fortaleza',
+            'Grêmio',
+            'Internacional',
+            'Juventude',
+            'Palmeiras',
+            'Santos',
+            'Sport Recife',
+            'São Paulo',
+        ]
+        assert obj.sort_by_name() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_sort_by_point(self, entrance, expected):
+        """Test sort."""
+        obj = CBF(entrance)
+        expected = [
+            'Chapecoense',
+            'Sport Recife',
+            'Bahia',
+            'Grêmio',
+            'Juventude',
+            'Athletico-PR',
+            'Cuiabá',
+            'São Paulo',
+            'Internacional',
+            'Santos',
+            'Ceará SC',
+            'Atlético-GO',
+            'América-MG',
+            'Fluminense',
+            'Bragantino',
+            'Corinthians',
+            'Fortaleza',
+            'Palmeiras',
+            'Flamengo',
+            'Atlético-MG',
+        ]
+        assert obj.sort_by_point() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_classify(self, entrance, expected):
+        """Test classify."""
+        obj = CBF(entrance)
+        expected = [
+            'Atlético-MG',
+            'Flamengo',
+            'Palmeiras',
+            'Fortaleza',
+            'Corinthians',
+            'Bragantino',
+            'Fluminense',
+            'América-MG',
+            'Atlético-GO',
+            'Ceará SC',
+            'Santos',
+            'Internacional',
+            'São Paulo',
+            'Cuiabá',
+            'Athletico-PR',
+            'Juventude',
+            'Grêmio',
+            'Bahia',
+            'Sport Recife',
+            'Chapecoense',
+        ]
+        assert obj.classify() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_libertadores(self, entrance, expected):
+        """Test classify."""
+        obj = CBF(entrance)
+        expected = [
+            'Atlético-MG',
+            'Flamengo',
+            'Palmeiras',
+            'Fortaleza',
+        ]
+        assert obj.classify_libertadores() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_qualific_libertadores(self, entrance, expected):
+        """Test classify."""
+        obj = CBF(entrance)
+        expected = [
+            'Corinthians',
+            'Bragantino',
+        ]
+        assert obj.qualify_libertadores() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_sulamericana(self, entrance, expected):
+        """Test classify."""
+        obj = CBF(entrance)
+        expected = [
+            'Fluminense',
+            'América-MG',
+            'Atlético-GO',
+            'Ceará SC',
+            'Santos',
+            'Internacional',
+        ]
+        assert obj.select_sulamericana() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test_case_0,
+    )
+    def test_rebaixados(self, entrance, expected):
+        """Test classify."""
+        obj = CBF(entrance)
+        expected = ['Grêmio', 'Bahia', 'Sport Recife', 'Chapecoense']
+        assert obj.rebaixados() == expected
+
+    @pytest.mark.parametrize(
+        'url_path entrance expected'.split(),
+        [
+            pytest.param(
+                'http://localhost:8000',
+                'Chapecoense',
+                15,
+                marks=pytest.mark.webtest,
+            ),
+            pytest.param(
+                'http://localhost:8000',
+                'Grêmio',
+                43,
+                marks=pytest.mark.webtest,
+            ),
+            pytest.param(
+                'http://localhost:8000',
+                'Bahia',
+                43,
+                marks=pytest.mark.webtest,
+            ),
+            pytest.param(
+                'http://localhost:8000',
+                'Sport Recife',
+                38,
+                marks=pytest.mark.webtest,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'Chapecoense',
+                15,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'Grêmio',
+                43,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'Bahia',
+                43,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'Sport Recife',
+                38,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'flamengo',
+                71,
+            ),
+            (
+                Path(__file__).parent.joinpath('index.html').as_posix(),
+                'atletico-mg',
+                84,
+            ),
+        ],
+    )
+    def test_por_nome(self, url_path, entrance, expected):
+        """Test classify."""
+        obj = CBF(url_path)
+        assert obj.clube(entrance) == expected

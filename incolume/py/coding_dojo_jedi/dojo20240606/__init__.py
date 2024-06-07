94f59f536fedef7157bbbc9fa5224512e8415d25
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-06-07 10:34:24 -0300

    feat: Código para fatorar, aplicado a estrutura

--- incolume/py/coding_dojo_jedi/dojo20240606/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240606/__init__.py
@@ -1,1 +1,37 @@
 """dojo module."""
+
+import logging
+import time
+from icecream import ic 
+import os
+
+
+ic.disable()
+if os.getenv('DEBUG_MODE'):
+    ic.enable()
+
+
+
+def calc_square0(l: list) -> list:  # noqa: E741
+    """Quadrado de cada elemento da lista."""
+    start = time.time()
+    result = []
+    for e in l:
+        result.append(e * e)  # noqa: PERF401
+    end = time.time()
+    logging.debug('calc_square executou em %s milisegundos', start - end)
+    return result
+
+
+def calc_cube0(l: list) -> list:  # noqa: E741
+    """Cubo de cada elemento da lista."""
+    start = time.time()
+    result = []
+    for e in l:
+        result.append(e * e * e)  # noqa: PERF401
+    end = time.time()
+    logging.debug('calc_cube executou em %s milisegundos', start - end)
+    return result
+
+
+def timeit(func: )
--- incolume/py/coding_dojo_jedi/dojo20240606/test_20240606.py
+++ incolume/py/coding_dojo_jedi/dojo20240606/test_20240606.py
@@ -1,18 +1,33 @@
 """Test module."""
 
 from typing import ClassVar, NoReturn
-import . as pkg
+import incolume.py.coding_dojo_jedi.dojo20240606 as pkg
 import pytest
+
 
 class TestCase:
     """Test case class."""
 
+    case_test_square: ClassVar = [
+        (range(5), [pow(x, 2) for x in range(5)]),
+    ]
+
+    case_test_cube: ClassVar = [
+        (list(range(5)), [pow(x, 3) for x in range(5)]),
+    ]
+
     @pytest.mark.parametrize(
         'entrance expected'.split(),
-        [
-             (None, None),
-        ],
+        case_test_square,
     )
-    def test_0(self, entrance, expected) -> NoReturn:
+    def test_square_0(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        assert pkg.calc_square0(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        case_test_cube,
+    )
+    def test_cube_0(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert pkg.calc_cube0(entrance) == expected

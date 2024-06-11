16b21d09404552ed839b354b558efd7a3c2573ad
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-06-11 15:59:00 -0300

    feat: Estrutura para dojo popular-sgbd-postgresql

---
+++ incolume/py/coding_dojo_jedi/dojo20250108/test_20250108.py
@@ -0,0 +1,30 @@
+"""Test module."""
+
+from __future__ import annotations
+from typing import ClassVar, NoReturn
+import incolume.py.coding_dojo_jedi.dojo20250108 as pkg
+import pytest
+
+
+class TestCase:
+    """Test case class."""
+
+    t0: ClassVar = None
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                None,
+                None,
+                marks=[
+                    pytest.mark.xpass(
+                        reason='Implementation failing (but shoulded ran)'
+                    )
+                ],
+            ),
+        ],
+    )
+    def test_0(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert pkg.dojo(entrance) == expected

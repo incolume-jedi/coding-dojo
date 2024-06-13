fa3f14f0feb36e9cf52fe3f457aeafc3d74cd70a
Britodfbr <britodfbr@gmail.com>
2024-06-13 10:34:37 -0300

    feat: Tratativas de sorteio (sample/shuffle)

--- incolume/py/coding_dojo_jedi/dojo20240613/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240613/__init__.py
@@ -1,1 +1,58 @@
 """dojo module."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pandas as pd
+from faker import Faker
+from icecream import ic
+import datetime as dt
+import random
+import secrets
+
+Faker.seed(1061)
+fake = Faker('pt_BR')
+
+
+def data_fake():
+    """Gen data fake for XLSX files."""
+    return (
+        (name := f'{fake.first_name()} {fake.last_name()} {fake.last_name()}'),
+        fake.cpf(),
+        f'{fake.slug(name)}@correios.com.br',
+    )
+
+
+def generate_dataframe(count: int = 0) -> pd.DataFrame:
+    """Generate dataframe."""
+    count = count or 10
+    title = 'nome cpf email'.split()
+    users = (data_fake() for _ in range(count))
+    df0 = pd.DataFrame(users, columns=title)
+    ic(df0)
+    return df0
+
+
+def write_xlsx(data: pd.DataFrame, filename: Path | None = None) -> bool:
+    """Write xlsx file."""
+    filename = filename or Path(__file__).parent / 'empregados.xlsx'
+    data.to_excel(filename, index=False)
+    return filename.is_file()
+
+
+def sorteio(filename: Path|None = None) -> Path:
+    """Lotery by xlsx file."""
+    filename = filename or Path(__file__).parent / 'empregados.xlsx'
+    ext = {'.xlsx': pd.read_excel}
+    fout: Path = filename.with_name(f'{filename.stem}{dt.datetime.now(tz=None).isoformat()}.xlsx')
+    df0 = ext.get(ic(filename.suffix))(filename)
+    ic(df0)
+
+    itens = list(df0.index)
+    random.shuffle(itens)
+    ic(itens)
+    ic(random.sample(list(df0.index), k=3))
+
+    return fout
+
--- incolume/py/coding_dojo_jedi/dojo20240613/test_20240613.py
+++ incolume/py/coding_dojo_jedi/dojo20240613/test_20240613.py
@@ -1,19 +1,58 @@
 """Test module."""
 
+from pathlib import Path
 from typing import NoReturn
+from unittest import mock
+
 import incolume.py.coding_dojo_jedi.dojo20240613 as pkg
 import pytest
+from tempfile import NamedTemporaryFile
 
 
 class TestCase:
     """Test case class."""
 
+    @pytest.mark.skip()
+    def test_0(self) -> NoReturn:
+        """Unittest."""
+        expected = (
+            'Nicolas Silva Silva',
+            '630.418.957-57',
+            'nicolas-sampaio-barros@correios.com.br',
+        )
+        with mock.patch(pkg.fake, return_value=mock.MagicMock()) as m:
+            m.last_name.return_value = 'Silva'
+            m.fake.cpf.return_value = expected[1]
+            m.fake.slug.return_value = 'nicolas-silva-silva'
+            assert pkg.data_fake() == expected
+
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
-            (None, None),
+            (15, (15, 3)),
+            (None, (10, 3)),
         ],
     )
-    def test_0(self, entrance, expected) -> NoReturn:
+    def test_1(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        assert pkg.generate_dataframe(entrance).shape == expected
+
+    @pytest.mark.parametrize(
+        'entrance',
+        [
+            {
+                'filename': Path(NamedTemporaryFile().name),
+                'data': pkg.generate_dataframe(),
+            },
+            {
+                'data': pkg.generate_dataframe(100),
+            },
+        ],
+    )
+    def test_2(self, entrance) -> NoReturn:
+        """Unittest."""
+        assert pkg.write_xlsx(**entrance)
+
+    def test_3(self) -> NoReturn:
+        """Unittest."""
+        assert pkg.sorteio() == ''

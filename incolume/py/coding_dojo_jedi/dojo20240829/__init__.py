6d69675fa6ac6856d9f297638575623e7a96cf20
Brito <britodfbr@users.noreply.github.com>
2024-08-29 13:14:56 +0000

    feat: Solução do dojo20240829: Descompactação de objetos de forma prática

---
+++ incolume/py/coding_dojo_jedi/dojo20240829/__init__.py
@@ -0,0 +1,37 @@
+"""dojo module."""
+
+import datetime as dt
+from dataclasses import asdict, dataclass, field
+
+
+@dataclass
+class Pessoa:
+    """Pessoa class."""
+
+    full_name: str
+    born_date: dt.datetime
+    city: str
+    first_name: str = field(default='')
+    last_name: str = field(default='')
+
+    def __post_init__(self):
+        """Post init."""
+        self.first_name = self.full_name.split()[0]
+        self.last_name = self.full_name.split()[-1]
+
+    def asdict(self):
+        """As dict self."""
+        return asdict(self)
+
+
+def dojo0(pessoa: Pessoa) -> str:
+    """Dojo solution."""
+    return (
+        'Meu nome é "{first_name}"'
+        ' nascido em "{born_date:%A %d de %B do ano %Y}"'
+        ' na cidade de "{city}"'
+    ).format(
+        **pessoa.asdict(),
+    )
+
+
---
+++ incolume/py/coding_dojo_jedi/dojo20240829/test_20240829.py
@@ -0,0 +1,70 @@
+"""Test module."""
+
+from typing import NoReturn
+import incolume.py.coding_dojo_jedi.dojo20240829 as pkg
+import pytest
+
+
+class TestCase:
+    """Test case class."""
+
+    def test_0(self) -> NoReturn:
+        """Unittest."""
+        entrance = (
+            'Jesus Cristo',
+            pkg.dt.datetime(1, 12, 25, 0, 0, 1),
+            'Nazaré',
+        )
+        assert isinstance(pkg.Pessoa(*entrance), pkg.Pessoa)
+
+    def test_1(self) -> NoReturn:
+        """Unittest."""
+        entrance = (
+            'Jesus Cristo',
+            pkg.dt.datetime(1, 12, 25, 0, 0, 1),
+            'Nazaré',
+        )
+        expected = {
+            'full_name': 'Jesus Cristo',
+            'born_date': pkg.dt.datetime(1, 12, 25, 0, 0, 1),
+            'city': 'Nazaré',
+            'first_name': 'Jesus',
+            'last_name': 'Cristo',
+        }
+        assert pkg.asdict(pkg.Pessoa(*entrance)) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            (
+                (
+                    'Jesus Cristo',
+                    pkg.dt.datetime(1, 12, 25, 0, 0, 1),
+                    'Nazaré',
+                ),
+                'Meu nome é "Jesus" nascido em '
+                '"Tuesday 25 de December do ano 1" na cidade de "Nazaré"',
+            ),
+        ],
+    )
+    def test_2(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert pkg.dojo0(pkg.Pessoa(*entrance)) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            (
+                (
+                    'Jesus Cristo',
+                    pkg.dt.datetime(1, 12, 25, 0, 0, 1),
+                    'Nazaré',
+                ),
+                'Meu nome é "Jesus" nascido em '
+                '"Tuesday 25 de December do ano 1" na cidade de "Nazaré"',
+            ),
+        ],
+    )
+    def test_3(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert pkg.dojo(pkg.Pessoa(*entrance)) == expected

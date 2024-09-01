1afc4273861e6c92989919b62a4165d3ba31d143
Brito <britodfbr@users.noreply.github.com>
2024-09-01 19:00:57 +0000

    refactor: Implementado `lru_cache` em incolume.py.coding_dojo_jedi.dojo20240510.is_narcisist

--- incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240510/__init__.py
@@ -5,12 +5,14 @@ import logging
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+from functools import lru_cache
 
 __author__ = '@britodfbr'  # pragma: no cover
 
 from collections.abc import Container
 
 
+@lru_cache
 def is_narcisist(num: int) -> bool:
     """Check if number is narcisist number."""
     result = sum(pow(int(n), len(str(num))) for n in str(num) if n.isdigit())

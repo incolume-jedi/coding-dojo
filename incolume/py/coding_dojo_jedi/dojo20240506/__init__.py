f1b33db6ff137ea527e917929ac60cd1690ece6e
Brito <britodfbr@users.noreply.github.com>
2024-09-04 03:11:37 +0000

    refactor: feliz -> is_feliz

--- incolume/py/coding_dojo_jedi/dojo20240506/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20240506/__init__.py
@@ -5,12 +5,12 @@
 __author__ = '@britodfbr'  # pragma: no cover
 
 
-def feliz(num: int) -> bool:
+def is_feliz(num: int) -> bool:
     """Verifica se número é feliz."""
     nums = [pow(int(n), 2) for n in str(num)]
     if (snum := sum(nums)) == 1:
         return True
     try:
-        return feliz(snum)
+        return is_feliz(snum)
     except RecursionError:
         return False

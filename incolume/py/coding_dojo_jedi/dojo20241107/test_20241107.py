6a26026b78b30b9e86531033f196f9d55954c8d5
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-11-08 11:28:33 -0300

    refactor: criado dataclass para dados e fake response para chamada HTTP

--- incolume/py/coding_dojo_jedi/dojo20241107/test_20241107.py
+++ incolume/py/coding_dojo_jedi/dojo20241107/test_20241107.py
@@ -1,32 +1,78 @@
 """Test module."""
 
-from typing import ClassVar, NoReturn
-
+from typing import Any
 import httpx
-import incolume.py.coding_dojo_jedi.dojo20241107 as pkg
 import pytest
 from http import HTTPStatus
+from dataclasses import dataclass, field
+import datetime as dt
+import pytz
+
+TZ = pytz.timezone('America/Sao_Paulo')
+
+
+@dataclass
+class Json:
+    """Response JSON."""
+
+    code: str
+    ask: float
+    codein: str = 'BRL'
+    name: str = ''
+    high: float = 0.0
+    low: float = 0.0
+    varBid: float = 0.0  # noqa: N815
+    pctChange: float = 0.0  # noqa: N815
+    bid: float = 0.0
+    timestamp: int = field(default=0, init=False)
+    create_date: dt.datetime = field(
+        default_factory=dt.datetime.now(tz=TZ).isoformat(),
+    )
+
+    def __post_init__(self):
+        """Post init."""
+        self.code = self.code.upper()
+        self.timestamp = (
+            dt.datetime.strptime(
+                self.create_date,
+                '%Y-%m-%dT%H:%M:%S.%f',
+            )
+            .astimezone(dt.timezone.tzname())
+            .timestamp()
+        )
+
+    def to_dict(self) -> dict[str, Any]:
+        """Dict."""
+        return {f'{self.code}{self.codein}': {'ask': '1.23'}}
 
 
 @pytest.fixture()
-def fake_response(status: HTTPStatus | None = None, json: dict | None = None):
+def fake_response(
+    status: HTTPStatus | None = None,
+    json: Json | None = None,
+) -> httpx.Response:
     """Fake response."""
     status = status or HTTPStatus.OK
-    json = json or {}
+    json = json.to_dict() or {}
     return httpx.Response(status_code=status, json=json)
 
 
 class TestCase:
     """Test case class."""
 
-    t0: ClassVar = None
+    # t0: ClassVar = None
 
-    @pytest.mark.parametrize(
-        'entrance expected'.split(),
-        [
-            (None, None),
-        ],
-    )
-    def test_0(self, entrance, expected) -> NoReturn:
-        """Unittest."""
-        assert pkg.dojo(entrance) == expected
+    def test_0(self):
+        """Unit test."""
+        j = Json(code='usd', high=1.11, low=0.01, ask=1.1)
+        assert j.to_dict() == {}
+
+    # @pytest.mark.parametrize(
+    #     'entrance expected'.split(),
+    #     [
+    #         ('usd', None),
+    #     ],
+    # )
+    # def test_0(self, entrance, expected) -> NoReturn:
+    #     """Unittest."""
+    #     assert pkg.dojo(entrance) == expected

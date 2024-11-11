d99c21e4b2cd9600fc991a62b4543a2b1f1cb691
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-11-11 11:03:24 -0300

    test: Estruturas suplementares para testes unitários

--- incolume/py/coding_dojo_jedi/dojo20241107/test_20241107.py
+++ incolume/py/coding_dojo_jedi/dojo20241107/test_20241107.py
@@ -8,6 +8,8 @@ from dataclasses import asdict, dataclass, field
 import datetime as dt
 import pytz
 import incolume.py.coding_dojo_jedi.dojo20241107 as pkg
+from functools import partial
+from time_machine import travel
 
 TZ = pytz.timezone('America/Sao_Paulo')
 
@@ -43,19 +45,22 @@ class Json:
         dict_values = {**self.__dict__}
         del dict_values['timestamp']
         o = Json(**dict_values)
-        o.create_date = self.create_date.isoformat()
+        o.create_date = self.create_date.isoformat(sep=' ')
         return {f'{self.code}{self.codein}': asdict(o)}
 
 
-@pytest.fixture
+@pytest.fixture()
 def fake_response(
     status: HTTPStatus | None = None,
     json: Json | dict | None = None,
 ) -> httpx.Response:
     """Fake response."""
     status = status or HTTPStatus.OK
-    json = {'USDBRL': {'ask': 123}}
+    json = Json.to_dict() or {'USDBRL': {'ask': 123}}
     return httpx.Response(status_code=status, json=json)
+
+
+resp_200 = partial(fake_response, status=HTTPStatus.OK)
 
 
 class TestCase0:
@@ -77,6 +82,93 @@ class TestCase0:
         assert set('code ask timestamp create_date'.split()).issubset(
             o.to_dict()['USDBRL'].keys(),
         )
+
+    @travel('2024-11-07 12:34:56')
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                {'code': 'usd', 'ask': 1.23},
+                {
+                    'USDBRL': {
+                        'code': 'USD',
+                        'ask': 1.23,
+                        'codein': 'BRL',
+                        'name': '',
+                        'high': 0.0,
+                        'low': 0.0,
+                        'varBid': 0.0,
+                        'pctChange': 0.0,
+                        'bid': 0.0,
+                        'timestamp': 1730993696,
+                        'create_date': '2024-11-07 12:34:56-03:00',
+                    },
+                },
+                marks=[
+                    # pytest.mark.skip(),
+                ],
+            ),
+            pytest.param(
+                {'code': 'eur', 'ask': 1.23},
+                {
+                    'EURBRL': {
+                        'code': 'EUR',
+                        'ask': 1.23,
+                        'codein': 'BRL',
+                        'name': '',
+                        'high': 0.0,
+                        'low': 0.0,
+                        'varBid': 0.0,
+                        'pctChange': 0.0,
+                        'bid': 0.0,
+                        'timestamp': 1730993696,
+                        'create_date': '2024-11-07 12:34:56-03:00',
+                    },
+                },
+            ),
+            pytest.param(
+                {'code': 'eth', 'ask': 1.23},
+                {
+                    'ETHBRL': {
+                        'code': 'ETH',
+                        'ask': 1.23,
+                        'codein': 'BRL',
+                        'name': '',
+                        'high': 0.0,
+                        'low': 0.0,
+                        'varBid': 0.0,
+                        'pctChange': 0.0,
+                        'bid': 0.0,
+                        'timestamp': 1730993696,
+                        'create_date': '2024-11-07 12:34:56-03:00',
+                    },
+                },
+                marks=[],
+            ),
+            pytest.param(
+                {'code': 'btc', 'ask': 1.23},
+                {
+                    'BTCBRL': {
+                        'code': 'BTC',
+                        'ask': 1.23,
+                        'codein': 'BRL',
+                        'name': '',
+                        'high': 0.0,
+                        'low': 0.0,
+                        'varBid': 0.0,
+                        'pctChange': 0.0,
+                        'bid': 0.0,
+                        'timestamp': 1730993696,
+                        'create_date': '2024-11-07 12:34:56-03:00',
+                    },
+                },
+                marks=[],
+            ),
+        ],
+    )
+    def test_3(self, entrance, expected):
+        """Unit test."""
+        assert Json(**entrance).to_dict() == expected
 
 
 class TestCase1:

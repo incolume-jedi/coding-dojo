a723e2fd7247450cdc5bb459b15f0918beba7391
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-11-13 13:27:28 -0300

    feat: Tratativa em resolver o dojo

--- incolume/py/coding_dojo_jedi/dojo20241112/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20241112/__init__.py
@@ -1,7 +1,125 @@
 """dojo module."""
 
+import http
+import os
+import urllib
+import urllib.parse
+from pathlib import Path
 
-def dojo(*args: str, **kwargs: str) -> dict[str]:
+import httpx
+import nest_asyncio
+from icecream import ic
+
+# permite chamadas async para jupyter
+nest_asyncio.apply()
+
+ic.disable()
+if os.getenv('DEBUG_MODE'):
+    ic.enable()
+
+urls = [
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18319/colleccao_leis_1808_parte1.pdf?sequence=4&isAllowed=y',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18319/colleccao_leis_1808_parte2.pdf?sequence=5',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18321/colleccao_leis_1809_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18325/colleccao_leis_1810_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18323/colleccao_leis_1811_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18322/colleccao_leis_1812_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18324/colleccao_leis_1813_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18326/colleccao_leis_1814_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18329/colleccao_leis_1815_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18330/colleccao_leis_1816_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18331/colleccao_leis_1817_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18332/colleccao_leis_1818_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18333/colleccao_leis_1819_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18335/colleccao_leis_1820_parte1.pdf?sequence=1',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18321/colleccao_leis_1809_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18325/colleccao_leis_1810_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18323/colleccao_leis_1811_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18322/colleccao_leis_1812_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18324/colleccao_leis_1813_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18326/colleccao_leis_1814_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18329/colleccao_leis_1815_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18330/colleccao_leis_1816_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18331/colleccao_leis_1817_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18332/colleccao_leis_1818_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18333/colleccao_leis_1819_parte2.pdf?sequence=2',
+    'https://bd.camara.leg.br/bd/bitstream/handle/bdcamara/18335/colleccao_leis_1820_parte2.pdf?sequence=2',
+]
+
+
+async def async_stream(url: str, output: str = ''):
+    output = Path(output) if output else Path('files')
+    output.mkdir(exist_ok=True)
+    file = output / Path(urllib.parse.urlsplit(url).path).name
+
+    with file.open('wb') as f:
+        async with httpx.AsyncClient() as client:
+            async with client.stream('GET', url) as r:
+                async for chunk in r.aiter_bytes():
+                    f.write(chunk)
+
+
+def sync_download(url: str, output: str = '') -> Path:
     """Dojo solution."""
-    kwargs['args'] = args
-    return kwargs
+    output = Path(output) if output else Path('files')
+    output.mkdir(exist_ok=True)
+    file = output / Path(urllib.parse.urlsplit(url).path).name
+    resp = httpx.get(url)
+
+    ic(resp.status_code)
+    ic(type(resp))
+    ic(resp.url)
+    ic(resp.next_request)
+    ic(resp.next_request.url)
+    ic(resp.headers)
+    ic(resp.headers['location'])
+
+    if resp.status_code == http.HTTPStatus.FOUND.value:
+        resp = httpx.get(resp.next_request.url)
+    file.write_bytes(resp.content)
+    ic(file.absolute())
+    return file
+
+
+async def stream_download(url: str, output: str = '') -> Path:
+    """Dojo solution."""
+    output = Path(output) if output else Path('files')
+    output.mkdir(exist_ok=True)
+    file = output / Path(urllib.parse.urlsplit(url).path).name
+
+    with file.open('wb') as f:
+        async with (
+            httpx.AsyncClient() as client,
+            client.stream('GET', url) as r,
+        ):
+            ic(r.status_code)
+            async for chunk in r.aiter_bytes():
+                f.write(chunk)
+    ic(file.absolute())
+    return file
+
+
+# async def async_download(*urls: str, *, output: str = '') -> dict[str]:
+#     """Dojo solution."""
+#     file = urllib.parse.urlsplit()
+#     output.mkdir(exist_ok=True)
+#     # file = output / Path(urllib.parse.urlsplit(url).path).name
+
+#     async def get_async(url: str):
+#         """Get async."""
+#         async with httpx.AsyncClient() as client:
+#             return await client.get(url)
+
+#     async def launch():
+#         """Launch async."""
+#         resps = await asyncio.gather(*map(get_async, urls))
+#         data = [resp.text for resp in resps]
+
+#         for html in data:
+#             ic(html)
+
+#     return file
+
+
+def dojo():
+    """Dojo."""
--- incolume/py/coding_dojo_jedi/dojo20241112/test_20241112.py
+++ incolume/py/coding_dojo_jedi/dojo20241112/test_20241112.py
@@ -3,19 +3,53 @@
 from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20241112 as pkg
 import pytest
+import asyncio
+
+pkg.ic.enable()
 
 
-class TestCase:
+class TestCase0:
+    """Test case class."""
+
+    url: ClassVar = 'https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/12.0/FreeBSD-12.0-RELEASE-amd64-mini-memstick.img'
+
+    def test_0(self):
+        """Unittest."""
+        assert pkg.async_stream(pkg.urls[0])
+
+
+class TestCase1:
     """Test case class."""
 
     t0: ClassVar = None
+    t1: ClassVar = [
+        'https://httpbin.org',
+    ]
+
+    def test_0(self):
+        """Unittest."""
+        assert pkg.sync_download(pkg.urls[0]).is_file()
+
+    def test_1(self):
+        """Unittest."""
+        assert asyncio.run(pkg.stream_download(pkg.urls[1])).absolute() == ''
 
     @pytest.mark.parametrize(
-        'entrance expected'.split(),
-        [
-            (None, None),
-        ],
+        'entrance',
+        pkg.urls,
     )
-    def test_0(self, entrance, expected) -> NoReturn:
+    def test_2(self, entrance) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        assert pkg.sync_download(entrance).is_file()
+
+    @pytest.mark.skip
+    @pytest.mark.parametrize(
+        'entrance',
+        pkg.urls,
+    )
+    def test_3(self, entrance) -> NoReturn:
+        """Unittest."""
+        assert pkg.async_download(entrance)
+
+
+pkg.ic.disable()

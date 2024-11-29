c1bf5438a66400b66ba2c0b86cf29330c83d12e6
Brito <britodfbr@users.noreply.github.com>
2024-11-29 21:30:41 +0000

    feat: tratativa em solucionar o dojo

--- incolume/py/coding_dojo_jedi/dojo20241128/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20241128/__init__.py
@@ -1,6 +1,24 @@
 """dojo module."""
 
-def dojo(*args: str, **kwargs: str)->dict[str]:
+from __future__ import annotations
+
+from typing import TypeAlias
+
+Board: TypeAlias = list[list[str]]
+
+
+def quadrant(sudoku: Board, x: int, y: int) -> Board:
+    """Find quadrant."""
+    xx = x // 3
+    yy = y // 3
+    return sudoku[xx * 3 : (xx + 1) * 3, yy * 3 : (yy + 1) * 3]
+
+
+def is_valid_sudoku(sudoku: Board) -> bool:
+    """Is validate sudoku."""
+    return False or sudoku
+
+
+def dojo(sudoku: Board) -> dict[str]:
     """Dojo solution."""
-    kwargs["args"] = args
-    return kwargs
+    return is_valid_sudoku(sudoku)

3b14d3cfeef0a2c780a69191709caccf7ca86374
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-12-14 09:07:58 -0300

    feat: tratativa #1 e respectivos testes

--- incolume/py/coding_dojo_jedi/dojo20241213/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20241213/__init__.py
@@ -1,6 +1,23 @@
 """dojo module."""
 
-def dojo(*args: str, **kwargs: str)->dict[str]:
-    """Dojo solution."""
-    kwargs["args"] = args
-    return kwargs
+from icecream import ic
+
+
+class Solution:
+    """Solution."""
+
+    def longest_consecutive(self, nums: list[int]) -> int:
+        """Dojo solution."""
+        result, temp = 1, 1
+        nums.sort()
+        ic(nums)
+        for idx, x in enumerate(nums):
+            ic(idx, x)
+            if x + 1 in nums and nums[idx + 1] == x + 1:
+                temp += 1
+                ic(temp)
+            elif temp > result:
+                ic(result)
+                result, temp = temp, 1
+                ic(result)
+        return result
--- incolume/py/coding_dojo_jedi/dojo20241213/test_20241213.py
+++ incolume/py/coding_dojo_jedi/dojo20241213/test_20241213.py
@@ -4,17 +4,24 @@ from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20241213 as pkg
 import pytest
 
-class TestCase:
+
+class TestLongestConsecutiveSequence:
     """Test case class."""
 
-    t0: ClassVar=None
+    t0: ClassVar = None
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
-             (None, None),
+            pytest.param([100, 4, 200, 1, 3, 2], 4),
+            pytest.param([0, 3, 7, 2, 5, 8, 4, 6, 0, 1], 9),
+            pytest.param(
+                [100, 4, 200, 201, 203, 202, 0, 3, 7, 2, 5, 8, 4, 6, 0, 1],
+                9,
+            ),
         ],
     )
     def test_0(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        obj = pkg.Solution()
+        assert obj.longest_consecutive(entrance) == expected

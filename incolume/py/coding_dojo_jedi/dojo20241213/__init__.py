c2e9a6538c2ad0b1c1b2329f52c578eb069d598f
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-12-14 09:29:15 -0300

    feat: Solução 2 e respectivos testes
    close #273

--- incolume/py/coding_dojo_jedi/dojo20241213/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20241213/__init__.py
@@ -3,7 +3,7 @@
 from icecream import ic
 
 
-class Solution:
+class Solutions:
     """Solution."""
 
     def longest_consecutive(self, nums: list[int]) -> int:
@@ -21,3 +21,19 @@ class Solution:
                 result, temp = temp, 1
                 ic(result)
         return result
+
+    def longest_consecutive1(self, nums: list[int]) -> int:
+        """Solution."""
+        nums_set = set(nums)
+        longest_streak = 0
+
+        for num in nums:
+            if num - 1 not in nums_set:
+                current_num = num
+                current_streak = 1
+            while current_num + 1 in nums_set:
+                current_num += 1
+                current_streak += 1
+            longest_streak = max(current_streak, longest_streak)
+
+        return longest_streak

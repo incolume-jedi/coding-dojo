b98af2b1b03f0ec04ec377c8fd0b0ca9f3a7b753
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2024-12-24 08:56:14 -0300

    feat: Solução para dojo20241221 - Sum of Subarray Minimums
    close #277

--- docs/user_guide/dojos-resolvidos.md
+++ docs/user_guide/dojos-resolvidos.md
@@ -8,7 +8,7 @@
 
 ---
 
-149 dojos resolvidos
+150 dojos resolvidos
 
 ---
 
@@ -16,6 +16,7 @@
 
 ### 2024
 
+ - [Dojo20241221 &#8212; Sum of Subarray Minimums](https://github.com/incolume-jedi/coding-dojo/tree/dev/incolume/py/coding_dojo_jedi/dojo20241221/README.md){:target="_blank"}
  - [Dojo20241220 &#8212; Revert String to image from json file.](https://github.com/incolume-jedi/coding-dojo/tree/dev/incolume/py/coding_dojo_jedi/dojo20241220/README.md){:target="_blank"}
  - [Dojo20241216 &#8212; Convert Image to String and write into json file.](https://github.com/incolume-jedi/coding-dojo/tree/dev/incolume/py/coding_dojo_jedi/dojo20241216/README.md){:target="_blank"}
  - [Dojo20241214 &#8212; Top K Frequent Elements](https://github.com/incolume-jedi/coding-dojo/tree/dev/incolume/py/coding_dojo_jedi/dojo20241214/README.md){:target="_blank"}
--- incolume/py/coding_dojo_jedi/dojo20241221/test_20241221.py
+++ incolume/py/coding_dojo_jedi/dojo20241221/test_20241221.py
@@ -13,10 +13,27 @@ class TestCase:
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
-            pytest.param([3, 1, 2, 4], []),
-            pytest.param([11, 81, 94, 43, 3], None),
+            pytest.param([3], 3, marks=[]),
+            pytest.param([3, 1], 5, marks=[]),
+            pytest.param([3, 1, 2], 9, marks=[pytest.mark.skip]),
+            pytest.param([3, 1, 2, 4], 17, marks=[pytest.mark.skip]),
+            pytest.param([11, 81, 94, 43, 3], 444, marks=[]),
         ],
     )
     def test_0(self, entrance, expected) -> NoReturn:
         """Unittest."""
         assert pkg.dojo(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param([3], 3, marks=[]),
+            pytest.param([3, 1], 5, marks=[]),
+            pytest.param([3, 1, 2], 9, marks=[]),
+            pytest.param([3, 1, 2, 4], 17, marks=[]),
+            pytest.param([11, 81, 94, 43, 3], 444, marks=[]),
+        ],
+    )
+    def test_1(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert pkg.sum_subarray_mins(entrance) == expected

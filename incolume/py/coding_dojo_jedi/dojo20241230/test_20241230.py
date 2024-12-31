318db946e9b76ac42ee9dd27c594126d46a29133
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-12-31 10:42:14 -0300

    refactor: Atualização da implementação

--- incolume/py/coding_dojo_jedi/dojo20241230/test_20241230.py
+++ incolume/py/coding_dojo_jedi/dojo20241230/test_20241230.py
@@ -4,15 +4,21 @@ from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20241230 as pkg
 from bs4 import BeautifulSoup
 import pytest
+from incolume.py.coding_dojo_jedi import utils
+from icecream import ic
+import regex
 
 
 class TestCase:
     """Test case class."""
 
     t0: ClassVar = None
-    content: ClassVar = """
+    content: ClassVar[str] = """
     <html>
       <body>
+        <p>
+          <a href="http://legislacao.planalto.gov.br/legisla/legislacao.nsf/Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
+        </p>
         <p>
           <a href="anexo/xpto.doc">anexo 1</a>
           <a href="anexo/xpto.docx">anexo 2</a>
@@ -20,7 +26,8 @@ class TestCase:
           <a href="anexo/xpto.xls">anexo 4</a>
           <a href="anexo/xpto.rtf">anexo 5</a>
           <a href="anexo/xpto.pdf">anexo 6</a>
-        </p><p>
+        </p>
+        <p>
           <a href="D11927.htm">
             Decreto nº 11.927, de 22 de fev de 2024
           </a>
@@ -28,10 +35,24 @@ class TestCase:
       </body>
     </html>"""
 
-    @pytest.mark.skip
-    def test_3(self) -> NoReturn:
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                'arquivo1.html',
+                regex.escape('[]'),
+                marks=[],
+            ),
+            pytest.param('arquivo2.htm', '', marks=[pytest.mark.skip]),
+            pytest.param('arquivo3.htm', '', marks=[pytest.mark.skip]),
+        ],
+    )
+    def test_3(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo()
+        filename = utils.pseudo_filename().with_name(entrance)
+        filename.write_text(self.content)
+        ic(filename)
+        assert (pkg.dojo(path_dir=filename.parents[0])) == expected
 
     def test_2(self) -> NoReturn:
         """Unittest."""

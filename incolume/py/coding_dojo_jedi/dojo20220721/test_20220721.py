e4eeb16a4a0a9fbda17c17d3d1f58033033f1c93
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-01-03 20:13:17 -0300

    test: Nova classe para testes mocked

--- incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
+++ incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
@@ -2,10 +2,27 @@
 
 # !/usr/bin/env python
 from os import environ
-
+import respx
 import pytest
-
+import httpx
 from incolume.py.coding_dojo_jedi.dojo20220721 import saudacao
+from http import HTTPStatus
+from typing import NoReturn
+
+
+class TestMocked:
+    """Mock Test case class."""
+
+    def test_0(self) -> NoReturn:
+        """Unittest."""
+
+    def test_context(self) -> NoReturn:
+        """Unittest."""
+        with respx.mock() as respx_mock:
+            my_route = respx_mock.get('https://example.org/')
+            response = httpx.get('https://example.org/')
+            assert my_route.called
+            assert response.status_code == HTTPStatus.OK
 
 
 @pytest.mark.webtest

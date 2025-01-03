1331ef8b62d8a882ea3227362fd3a4c9c933ce00
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-01-03 20:16:13 -0300

    fix: implementação de prospecção mock em HTTP

--- incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
+++ incolume/py/coding_dojo_jedi/dojo20220721/test_20220721.py
@@ -8,6 +8,7 @@ import httpx
 import incolume.py.coding_dojo_jedi.dojo20220721 as pkg
 from http import HTTPStatus
 from typing import NoReturn
+import json
 
 
 class TestMocked:
@@ -23,6 +24,31 @@ class TestMocked:
             response = httpx.get('https://example.org/')
             assert my_route.called
             assert response.status_code == HTTPStatus.OK
+
+    def test_context_1(self) -> NoReturn:
+        """Unittest."""
+        with respx.mock() as respx_mock:
+            my_route = respx_mock.get(pkg.url)
+            response = httpx.get(pkg.url)
+            assert my_route.called
+            assert response.status_code == HTTPStatus.OK
+
+    def test_context_2(self) -> NoReturn:
+        """Unittest."""
+        with respx.mock() as respx_mock:
+            my_route = respx_mock.get(pkg.url).mock(
+                return_value=httpx.Response(
+                    200,
+                    text='Baz',
+                    json=json.loads(json.dumps({'b': 'Baz'})),
+                ),
+            )
+            response = httpx.get(pkg.url)
+            assert my_route.called
+            assert response.status_code == HTTPStatus.OK
+            assert response.text == 'Baz'
+            assert response.content == b'Baz'
+            assert response.json() == ''
 
 
 @pytest.mark.webtest

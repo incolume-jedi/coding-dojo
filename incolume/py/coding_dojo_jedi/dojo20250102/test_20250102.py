371b10c2272e42a011918cdbd68f289dd084c359
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-01-03 09:49:51 -0300

    test: Normalização de testes com a nova base fake para testes

--- incolume/py/coding_dojo_jedi/dojo20250102/test_20250102.py
+++ incolume/py/coding_dojo_jedi/dojo20250102/test_20250102.py
@@ -86,6 +86,7 @@ Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
       </body>
     </html>"""
 
+    @pytest.mark.skip
     def test_find_list_ahref_files(self):
         """Unit test."""
         entrance = utils.pseudo_filename()
@@ -109,12 +110,14 @@ Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
         entrance = utils.pseudo_filename()
         expected = [
             '<a href="anexo/xpto.doc">anexo 1</a>',
-            '<a href="anexo/xpto.docx">anexo 2</a>',
-            '<a href="anexo/xpto.docx">anexo 2</a>',
-            '<a href="anexo/xpto.rtf">anexo 5</a>',
-            '<a href="anexo/xpto.xlsx">anexo 3</a>',
-            '<a href="anexo/xpto.xls">anexo 4</a>',
-            '<a href="anexo/xpto.xlsx">anexo 3</a>',
+            '<a href="anexo/xpto.docx">anexo 4</a>',
+            '<a href="anexo/xpto.docx">anexo 4</a>',
+            '<a href="anexo/xpto.rtf">anexo 13</a>',
+            '<a href="anexo/xpto.rtf">anexo 14</a>',
+            '<a href="anexo/xpto.rtf">anexo 15</a>',
+            '<a href="anexo/xpto.xlsx">anexo 7</a>',
+            '<a href="anexo/xpto.xls">anexo 10</a>',
+            '<a href="anexo/xpto.xlsx">anexo 7</a>',
             '<a href="http://legislacao.planalto.gov.br/legisla/legislacao.nsf/'
             '\nViw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>',
         ]
@@ -129,16 +132,46 @@ Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
                 'arquivo1.html',
                 [
                     '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.NSF/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.Nsf/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
                     'legislacao.nsf/\nViw_Identificacao/ACP%2031-1966?'
                     'OpenDocument"> Link </a>',
                     '<a href="anexo/xpto.doc">anexo 1</a>',
-                    '<a href="anexo/xpto.docx">anexo 2</a>',
-                    '<a href="anexo/xpto.xlsx">anexo 3</a>',
-                    '<a href="anexo/xpto.xls">anexo 4</a>',
-                    '<a href="anexo/xpto.rtf">anexo 5</a>',
-                    '<a href="anexo/xpto.pdf">anexo 6</a>',
-                    '<a href="D11927.htm">\n            Decreto nº 11.927, de'
-                    ' 22 de fev de 2024\n          </a>',
+                    '<a href="anexo/xpto.Doc">anexo 2</a>',
+                    '<a href="anexo/xpto.DOC">anexo 3</a>',
+                    '<a href="anexo/xpto.docx">anexo 4</a>',
+                    '<a href="anexo/xpto.Docx">anexo 5</a>',
+                    '<a href="anexo/xpto.DOCX">anexo 6</a>',
+                    '<a href="anexo/xpto.xlsx">anexo 7</a>',
+                    '<a href="anexo/xpto.XLSX">anexo 8</a>',
+                    '<a href="anexo/xpto.Xlsx">anexo 9</a>',
+                    '<a href="anexo/xpto.xls">anexo 10</a>',
+                    '<a href="anexo/xpto.Xls">anexo 11</a>',
+                    '<a href="anexo/xpto.XLS">anexo 12</a>',
+                    '<a href="anexo/xpto.rtf">anexo 13</a>',
+                    '<a href="anexo/xpto.rtf">anexo 14</a>',
+                    '<a href="anexo/xpto.rtf">anexo 15</a>',
+                    '<a href="anexo/xpto.pdf">anexo 16</a>',
+                    '<a href="anexo/xpto.Pdf">anexo 17</a>',
+                    '<a href="anexo/xpto.PDF">anexo 18</a>',
+                    '<a href="D11923.htm">\n            '
+                    'Decreto nº 11.923, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11924.Htm">\n            '
+                    'Decreto nº 11.924, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11925.HTM">\n            '
+                    'Decreto nº 11.925, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11926.html">\n            '
+                    'Decreto nº 11.926, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11927.Html">\n            '
+                    'Decreto nº 11.927, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11928.htmL">\n            '
+                    'Decreto nº 11.928, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11929.HTML">\n            '
+                    'Decreto nº 11.929, de 22 de fev de 2024\n          </a>',
                 ],
                 marks=[],
             ),
@@ -146,16 +179,46 @@ Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
                 'arquivo2.htm',
                 [
                     '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.NSF/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.Nsf/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
                     'legislacao.nsf/\nViw_Identificacao/ACP%2031-1966?'
                     'OpenDocument"> Link </a>',
                     '<a href="anexo/xpto.doc">anexo 1</a>',
-                    '<a href="anexo/xpto.docx">anexo 2</a>',
-                    '<a href="anexo/xpto.xlsx">anexo 3</a>',
-                    '<a href="anexo/xpto.xls">anexo 4</a>',
-                    '<a href="anexo/xpto.rtf">anexo 5</a>',
-                    '<a href="anexo/xpto.pdf">anexo 6</a>',
-                    '<a href="D11927.htm">\n            Decreto nº 11.927,'
-                    ' de 22 de fev de 2024\n          </a>',
+                    '<a href="anexo/xpto.Doc">anexo 2</a>',
+                    '<a href="anexo/xpto.DOC">anexo 3</a>',
+                    '<a href="anexo/xpto.docx">anexo 4</a>',
+                    '<a href="anexo/xpto.Docx">anexo 5</a>',
+                    '<a href="anexo/xpto.DOCX">anexo 6</a>',
+                    '<a href="anexo/xpto.xlsx">anexo 7</a>',
+                    '<a href="anexo/xpto.XLSX">anexo 8</a>',
+                    '<a href="anexo/xpto.Xlsx">anexo 9</a>',
+                    '<a href="anexo/xpto.xls">anexo 10</a>',
+                    '<a href="anexo/xpto.Xls">anexo 11</a>',
+                    '<a href="anexo/xpto.XLS">anexo 12</a>',
+                    '<a href="anexo/xpto.rtf">anexo 13</a>',
+                    '<a href="anexo/xpto.rtf">anexo 14</a>',
+                    '<a href="anexo/xpto.rtf">anexo 15</a>',
+                    '<a href="anexo/xpto.pdf">anexo 16</a>',
+                    '<a href="anexo/xpto.Pdf">anexo 17</a>',
+                    '<a href="anexo/xpto.PDF">anexo 18</a>',
+                    '<a href="D11923.htm">\n            '
+                    'Decreto nº 11.923, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11924.Htm">\n            '
+                    'Decreto nº 11.924, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11925.HTM">\n            '
+                    'Decreto nº 11.925, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11926.html">\n            '
+                    'Decreto nº 11.926, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11927.Html">\n            '
+                    'Decreto nº 11.927, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11928.htmL">\n            '
+                    'Decreto nº 11.928, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11929.HTML">\n            '
+                    'Decreto nº 11.929, de 22 de fev de 2024\n          </a>',
                 ],
                 marks=[],
             ),
@@ -163,16 +226,46 @@ Viw_Identificacao/ACP%2031-1966?OpenDocument"> Link </a>
                 'arquivo3.htm',
                 [
                     '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.NSF/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
+                    'legislacao.Nsf/\nViw_Identificacao/ACP%2031-1966?'
+                    'OpenDocument"> Link </a>',
+                    '<a href="http://legislacao.planalto.gov.br/legisla/'
                     'legislacao.nsf/\nViw_Identificacao/ACP%2031-1966?'
                     'OpenDocument"> Link </a>',
                     '<a href="anexo/xpto.doc">anexo 1</a>',
-                    '<a href="anexo/xpto.docx">anexo 2</a>',
-                    '<a href="anexo/xpto.xlsx">anexo 3</a>',
-                    '<a href="anexo/xpto.xls">anexo 4</a>',
-                    '<a href="anexo/xpto.rtf">anexo 5</a>',
-                    '<a href="anexo/xpto.pdf">anexo 6</a>',
-                    '<a href="D11927.htm">\n            Decreto nº 11.927,'
-                    ' de 22 de fev de 2024\n          </a>',
+                    '<a href="anexo/xpto.Doc">anexo 2</a>',
+                    '<a href="anexo/xpto.DOC">anexo 3</a>',
+                    '<a href="anexo/xpto.docx">anexo 4</a>',
+                    '<a href="anexo/xpto.Docx">anexo 5</a>',
+                    '<a href="anexo/xpto.DOCX">anexo 6</a>',
+                    '<a href="anexo/xpto.xlsx">anexo 7</a>',
+                    '<a href="anexo/xpto.XLSX">anexo 8</a>',
+                    '<a href="anexo/xpto.Xlsx">anexo 9</a>',
+                    '<a href="anexo/xpto.xls">anexo 10</a>',
+                    '<a href="anexo/xpto.Xls">anexo 11</a>',
+                    '<a href="anexo/xpto.XLS">anexo 12</a>',
+                    '<a href="anexo/xpto.rtf">anexo 13</a>',
+                    '<a href="anexo/xpto.rtf">anexo 14</a>',
+                    '<a href="anexo/xpto.rtf">anexo 15</a>',
+                    '<a href="anexo/xpto.pdf">anexo 16</a>',
+                    '<a href="anexo/xpto.Pdf">anexo 17</a>',
+                    '<a href="anexo/xpto.PDF">anexo 18</a>',
+                    '<a href="D11923.htm">\n            '
+                    'Decreto nº 11.923, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11924.Htm">\n            '
+                    'Decreto nº 11.924, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11925.HTM">\n            '
+                    'Decreto nº 11.925, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11926.html">\n            '
+                    'Decreto nº 11.926, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11927.Html">\n            '
+                    'Decreto nº 11.927, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11928.htmL">\n            '
+                    'Decreto nº 11.928, de 22 de fev de 2024\n          </a>',
+                    '<a href="D11929.HTML">\n            '
+                    'Decreto nº 11.929, de 22 de fev de 2024\n          </a>',
                 ],
                 marks=[],
             ),

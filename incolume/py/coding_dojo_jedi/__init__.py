c4c4f73dd59b475f3c1541667379ee57b6370e33
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-01-13 19:10:51 -0300

    fix: Tratamento de exceção para carregamento do módulo
    close #332

--- incolume/py/coding_dojo_jedi/__init__.py
+++ incolume/py/coding_dojo_jedi/__init__.py
@@ -3,19 +3,23 @@
 import logging
 from pathlib import Path
 
+from icecream import ic
 from toml import load
 
 __author__ = '@britodfbr'
 
 configfile = Path(__file__).parents[3].joinpath('pyproject.toml')
 versionfile = Path(__file__).parent.joinpath('version.txt')
-
-versionfile.write_text(
-    f'{load(configfile)["tool"]["poetry"]["version"]}\n',
-)
+try:
+    versionfile.write_text(
+        f'{load(configfile)["tool"]["poetry"]["version"]}\n',
+    )
+except FileNotFoundError:
+    pass
 
 __version__ = versionfile.read_text(encoding='utf-8').strip()
 
+
 if __name__ == '__main__':
-    logging.debug('%s, %s', configfile, versionfile)
-    logging.debug('Vesion load: %s', __version__)
+    logging.debug(ic('%s, %s', configfile, versionfile))
+    logging.debug(ic('Vesion load: %s', __version__))

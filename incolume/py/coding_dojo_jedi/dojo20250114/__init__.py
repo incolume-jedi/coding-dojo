f76048f65c0e58142b591b28f5cc81ac3da15469
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-01-20 07:11:28 -0300

    feat: Solução para dojo

--- incolume/py/coding_dojo_jedi/dojo20250114/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20250114/__init__.py
@@ -1,14 +1,22 @@
 """dojo module."""
 
 from __future__ import annotations
-from incolume.py.coding_dojo_jedi.dojo20250106 import PreprocessImageOCR
 
+from pathlib import Path
+from typing import Self
+
+import cv2
+from incolume.py.coding_dojo_jedi.dojo20250106 import (
+    PreprocessImageOCR,
+)
+
+IMG_DIR: Path = Path(__file__).parents[1] / 'generic_data' / 'text_img'
 
 
 class PPIOCR(PreprocessImageOCR):
     """Class rescaling."""
 
-def dojo(*args: str, **kwargs: str) -> dict[str]:
-    """Dojo solution."""
-    kwargs['args'] = args
-    return kwargs
+    def grayscale(self) -> Self:
+        """Gray scale image."""
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        return self
--- incolume/py/coding_dojo_jedi/dojo20250114/test_20250114.py
+++ incolume/py/coding_dojo_jedi/dojo20250114/test_20250114.py
@@ -4,19 +4,22 @@ from __future__ import annotations
 from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20250114 as pkg
 import pytest
+from pathlib import Path
 
 
 class TestCase:
     """Test case class."""
 
-    t0: ClassVar = None
+    obj: ClassVar[pkg.PPIOCR] = pkg.PPIOCR()
+    img0: Path = pkg.IMG_DIR / 'letter.png'
+    img1: Path = pkg.IMG_DIR / 'ctr-1808-08-25.png'
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
             pytest.param(
-                None,
-                None,
+                img0,
+                set('coding-dojo letter_latest.png'.split()),
                 marks=[
                     pytest.mark.xpass(
                         reason='Implementation failing (but shoulded ran)',
@@ -25,6 +28,12 @@ class TestCase:
             ),
         ],
     )
-    def test_0(self, entrance, expected) -> NoReturn:
+    def test_gray_scale(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        self.obj.img_path = entrance
+        self.obj.grayscale()
+        result = self.obj.save()
+        assert self.obj.img_path == entrance
+        assert self.obj.img is not None
+        assert result.is_file()
+        assert expected.issubset(result.parts)

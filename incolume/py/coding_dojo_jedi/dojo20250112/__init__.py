d9a1feb96c71cbb95e2b5c69d07084ce6cb23ec9
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-01-22 20:18:08 -0300

    feat: implementações e testes
    close #329

--- incolume/py/coding_dojo_jedi/dojo20250112/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20250112/__init__.py
@@ -2,7 +2,112 @@
 
 from __future__ import annotations
 
-def dojo(*args: str, **kwargs: str)->dict[str]:
-    """Dojo solution."""
-    kwargs["args"] = args
-    return kwargs
+from functools import lru_cache
+from itertools import count
+
+
+def is_prime_0(num: int | bytes | str) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+
+    if num <= 1:
+        return False
+    for x in range(2, num):  # noqa: SIM110
+        if num != 2 and (num % x) == 0:
+            return False
+    return True
+
+
+def is_prime_1(num: int | bytes | str) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+
+    if num <= 1:
+        return False
+    for x in range(3, num, 2):  # noqa: SIM110
+        if num != 2 and (num % x) == 0:
+            return False
+    return True
+
+
+@lru_cache
+def is_prime_2(num: int) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+    if num <= 1:
+        return False
+
+    for x in range(2, (num // 2) + 1):  # noqa: SIM110
+        if (num % x) == 0:
+            return False
+    return True
+
+
+def is_prime_3(num: int) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+    if num <= 1:
+        return False
+    return all(num % x != 0 for x in range(2, num // 2 + 1))
+
+
+@lru_cache
+def is_prime_4(num: int) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+    if num <= 1:
+        return False
+    return all(num % n != 0 for n in range(2, num // 2 + 1))
+
+
+@lru_cache
+def is_prime(num: int) -> bool:
+    """Check if prime."""
+    try:
+        if isinstance(num, float):
+            if not (num == int(num)):
+                raise ValueError
+        num = int(num)
+    except (TypeError, ValueError):
+        return False
+    if num <= 1:
+        return False
+    return all(num % n != 0 for n in range(3, int(num**(1/2)) + 1), 2)
+
+
+def gen_prime():
+    """Prime generator."""
+    counter = count(1)
+    while True:
+        if is_prime1(n := next(counter)):
+            yield n
--- incolume/py/coding_dojo_jedi/dojo20250112/test_20250112.py
+++ incolume/py/coding_dojo_jedi/dojo20250112/test_20250112.py
@@ -1,21 +1,82 @@
 """Test module."""
 
 from __future__ import annotations
-from typing import ClassVar, NoReturn
+from typing import ClassVar
 import incolume.py.coding_dojo_jedi.dojo20250112 as pkg
 import pytest
 
-class TestCase:
-    """Test case class."""
 
-    t0: ClassVar=None
+class TestPrimes:
+    """Case teste."""
+
+    test0: ClassVar = [
+        (0o2, True),  # octal
+        (0x2, True),  # Hexadecimal
+        (0b11, True),  # binário
+        (3j, False),  # complexo
+        (2.0, True),  # float
+        (2.1, False),  # float
+        (b'2', True),  # bytes
+        ('2', True),  # str
+        (10**5, False),
+        (9323, True),
+        (1, False),
+        (2, True),
+        (3, True),
+        (4, False),
+        (41, True),
+        (51, False),
+        (71, True),
+        (59, True),
+        (89, True),
+    ]
+
+    @pytest.mark.skip
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test0,
+    )
+    def test_is_prime_0(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime_0(entrance) == expected
+
+    @pytest.mark.skip
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test0,
+    )
+    def test_is_prime_1(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime_1(entrance) == expected
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
-        [
-             pytest.param(None, None, marks=[pytest.mark.xpass(reason="Implementation failing (but shoulded ran)")]),
-        ],
+        test0,
     )
-    def test_0(self, entrance, expected) -> NoReturn:
-        """Unittest."""
-        assert pkg.dojo(entrance) == expected
+    def test_is_prime_2(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime_2(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test0,
+    )
+    def test_is_prime_3(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime_3(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test0,
+    )
+    def test_is_prime_4(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime_4(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        test0,
+    )
+    def test_is_prime(self, entrance, expected):
+        """Check is prime number."""
+        assert pkg.is_prime(entrance) == expected

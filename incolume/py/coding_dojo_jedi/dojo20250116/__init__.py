57ac8c392029a1327d3eec30900e175df5565de1
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-01-23 13:40:24 -0300

    refactor: implementação parcial

--- incolume/py/coding_dojo_jedi/dojo20250116/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20250116/__init__.py
@@ -2,8 +2,28 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 
-def dojo(*args: str, **kwargs: str) -> dict[str]:
+from icecream import ic
+from incolume.py.coding_dojo_jedi.dojo20250114 import PreprocessImageOCR
+
+IMG_DIR: Path = Path(__file__).parents[1] / 'generic_data' / 'text_img'
+
+
+class PPImg(PreprocessImageOCR):
+    """Class PPImg."""
+
+    def __repr__(self) -> str:
+        """Repr class."""
+        return f'{self.__class__.__name__}({self.__dict__})'
+
+
+def dojo(**kwargs: dict[str, Path]) -> dict[str]:
     """Dojo solution."""
-    kwargs['args'] = args
-    return kwargs
+    obj = PPImg(**kwargs) or kwargs.get('obj')
+    ic(obj)
+    return obj
+
+
+if __name__ == '__main__':
+    dojo()

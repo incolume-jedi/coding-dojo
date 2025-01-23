5d1943c11b87d6f8c63e13780c04465b214d9dbb
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-01-23 13:40:24 -0300

    refactor: implementação parcial

--- incolume/py/coding_dojo_jedi/dojo20250116/test_20250116.py
+++ incolume/py/coding_dojo_jedi/dojo20250116/test_20250116.py
@@ -1,6 +1,7 @@
 """Test module."""
 
 from __future__ import annotations
+from pathlib import Path
 from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20250116 as pkg
 import pytest
@@ -10,21 +11,25 @@ class TestCase:
     """Test case class."""
 
     t0: ClassVar = None
+    img0: Path = pkg.IMG_DIR / 'letter.jpg'
+    img1: Path = pkg.IMG_DIR / 'ctr-1808-08-25.png'
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
             pytest.param(
-                None,
+                {},
                 None,
                 marks=[
                     pytest.mark.xpass(
-                        reason='Implementation failing (but shoulded ran)'
-                    )
+                        reason='Implementation failing (but shoulded ran)',
+                    ),
                 ],
             ),
         ],
     )
     def test_0(self, entrance, expected) -> NoReturn:
         """Unittest."""
-        assert pkg.dojo(entrance) == expected
+        obj = pkg.PPImg(self.img0)
+        assert obj == ''
+        assert pkg.dojo(**entrance) == expected

889d9f24a9c1f624d2001c455458565d2a1edd6d
Britodfbr <britodfbr@gmail.com>
2026-06-20 16:03:49 -0300

    feat: #367 primeira implementação em jupyter

--- incolume/py/coding_dojo_jedi/dojo20260620/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20260620/__init__.py
@@ -2,8 +2,19 @@
 
 from __future__ import annotations
 
+from icecream import ic
+
 
 def dojo(*args: str, **kwargs: str) -> dict[str]:
     """Dojo solution."""
     kwargs['args'] = args
     return kwargs
+
+
+def main():
+    """Main function."""
+    ic('Hello from dojo20260620!')
+
+
+if __name__ == '__main__':
+    main()

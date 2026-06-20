409693346e46151c3c421987de3d56a12022bba7
Britodfbr <britodfbr@gmail.com>
2026-06-20 12:55:46 -0300

    feat: #367 estrutura para novo dojo

---
+++ incolume/py/coding_dojo_jedi/dojo20260620/__init__.py
@@ -0,0 +1,9 @@
+"""dojo module."""
+
+from __future__ import annotations
+
+
+def dojo(*args: str, **kwargs: str) -> dict[str]:
+    """Dojo solution."""
+    kwargs['args'] = args
+    return kwargs

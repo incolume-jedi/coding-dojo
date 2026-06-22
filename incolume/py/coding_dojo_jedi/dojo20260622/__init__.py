97fa1a0672e8b9389f8bfb69ceaea036d729dd62
Britodfbr <britodfbr@gmail.com>
2026-06-22 10:22:18 -0300

    feat: #368 implementação e respectivos testes

--- incolume/py/coding_dojo_jedi/dojo20260622/__init__.py
+++ incolume/py/coding_dojo_jedi/dojo20260622/__init__.py
@@ -2,19 +2,42 @@
 
 from __future__ import annotations
 
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import qrcode
 from icecream import ic
 
 
-def dojo(*args: str, **kwargs: str)->dict[str]:
+def gen_qrcode(url: str = '', flout: Path | str = '') -> Path:
+    """Gerador de QR code."""
+    # Cria o QR code com a url informada ou com a url padrão
+    url = url or 'http://brito.blog.incolume.com.br'
+    imagem = qrcode.make(url)
+
+    flout: Path = (
+        Path(flout)
+        if flout
+        else Path(NamedTemporaryFile(prefix='qrcode_', suffix='.png').name)
+    )
+
+    # Salva o arquivo gerado
+    imagem.save(flout)
+
+    return flout
+
+
+def dojo(*args: str, **kwargs: str) -> dict[str]:
     """Dojo solution."""
-    kwargs["args"] = args
+    kwargs['args'] = args
     return kwargs
+
 
 def main():
     """Main function."""
+    ic('Hello from dojo20260622!')
+    ic(gen_qrcode())
 
-    ic("Hello from dojo20260622!")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- incolume/py/coding_dojo_jedi/dojo20260622/test_20260622.py
+++ incolume/py/coding_dojo_jedi/dojo20260622/test_20260622.py
@@ -5,15 +5,39 @@ from typing import ClassVar, NoReturn
 import incolume.py.coding_dojo_jedi.dojo20260622 as pkg
 import pytest
 
+
 class TestCase:
     """Test case class."""
 
-    t0: ClassVar=None
+    t0: ClassVar = None
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
         [
-             pytest.param(None, None, marks=[pytest.mark.xfail(reason="Implementation failing (but shoulded ran)")]),
+            pytest.param(
+                {'url': 'http://brito.blog.incolume.com.br', 'flout': ''},
+                True,
+                marks=[],
+            ),
+        ],
+    )
+    def test_gen_qrcode(self, entrance, expected) -> NoReturn:
+        """Test gen_qrcode."""
+        result = pkg.gen_qrcode(**entrance)
+        assert result.exists() == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                None,
+                None,
+                marks=[
+                    pytest.mark.xfail(
+                        reason='Implementation failing (but shoulded ran)'
+                    )
+                ],
+            ),
         ],
     )
     def test_0(self, entrance, expected) -> NoReturn:

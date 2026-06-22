1f1d0b19baffe4a3cc0561211babbeee4749efc4
Britodfbr <britodfbr@gmail.com>
2026-06-22 09:58:34 -0300

    feat: #368 estrutura do dojo acrescentada

---
+++ incolume/py/coding_dojo_jedi/dojo20260622/__init__.py
@@ -0,0 +1,20 @@
+"""dojo module."""
+
+from __future__ import annotations
+
+from icecream import ic
+
+
+def dojo(*args: str, **kwargs: str)->dict[str]:
+    """Dojo solution."""
+    kwargs["args"] = args
+    return kwargs
+
+def main():
+    """Main function."""
+
+    ic("Hello from dojo20260622!")
+
+
+if __name__ == "__main__":
+    main()

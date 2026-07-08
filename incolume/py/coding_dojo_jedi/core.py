bb4c0b95a21ecfdcba3cfcf843e88e98afba5006
Britodfbr <britodfbr@gmail.com>
2026-07-08 10:51:41 -0300

    refactor: #374 implementação para sincronizar vesão `uv` e `poetry`

--- incolume/py/coding_dojo_jedi/core.py
+++ incolume/py/coding_dojo_jedi/core.py
@@ -6,9 +6,9 @@ from os import getenv
 from pathlib import Path
 
 from icecream import ic
-from toml import load
+from toml import load, dump
 
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
 
 logger = logging.getLogger(__name__)
 
@@ -16,11 +16,11 @@ logger = logging.getLogger(__name__)
 def is_debug_var_actived() -> bool:
     """Check environment variables for debug mode."""
     debug: bool = any(
-        getenv(x, '').casefold() in {'1', 'true', 'on'}
-        for x in ('INCOLUME_DEBUG_MODE', 'DEBUG_MODE', 'DEBUG')
+        getenv(x, "").casefold() in {"1", "true", "on"}
+        for x in ("INCOLUME_DEBUG_MODE", "DEBUG_MODE", "DEBUG")
     )
 
-    logger.debug(ic(f'Var Debug Mode: {debug}'))
+    logger.debug(ic(f"Var Debug Mode: {debug}"))
 
     return debug
 
@@ -37,23 +37,22 @@ def is_debug_enabled() -> bool:
     return debug
 
 
-configfile = Path(__file__).parents[3].joinpath('pyproject.toml')
-versionfile = Path(__file__).parent.joinpath('version.txt')
+configfile = Path(__file__).parents[3].joinpath("pyproject.toml")
+versionfile = Path(__file__).parent.joinpath("version.txt")
 
 with contextlib.suppress(FileNotFoundError):
-    versionfile.write_text(
-        f'{load(configfile)["tool"]["poetry"]["version"]}\n',
-    )
+    config_content = load(configfile)
 
-with contextlib.suppress(FileNotFoundError):
-    versionfile.write_text(
-        f'{load(configfile)["project"]["version"]}\n',
-    )
+current_version = config_content["project"]["version"]
 
-__version__ = versionfile.read_text(encoding='utf-8').strip()
-module_name = __name__.split('.')[0]
+config_content["tool"]["poetry"]["version"] = current_version
+dump(config_content, configfile.open("w", encoding="utf-8"))
+versionfile.write_text(f"{current_version}\n", encoding="utf-8")
+
+__version__ = versionfile.read_text(encoding="utf-8").strip()
+module_name = __name__.split(".")[0]
 project = Path(__file__).parents[3].name
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     is_debug_enabled()

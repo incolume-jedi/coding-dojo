d5095189581a02c9b46b13f956683da027157851
Britodfbr <britodfbr@gmail.com>
2026-07-08 09:03:25 -0300

    style(refactor): #371 formatação parcial de código

--- incolume/py/coding_dojo_jedi/dojo20220914/test_20220914.py
+++ incolume/py/coding_dojo_jedi/dojo20220914/test_20220914.py
@@ -8,30 +8,30 @@ from incolume.py.coding_dojo_jedi.dojo20220914.dojo20220914 import (
 )
 
 unittests = [
-    (("sábado", 1), "domingo"),
-    (("sábado", 2), "segunda-feira"),
-    (("sábado", 3), "terça-feira"),
-    (("sábado", 4), "quarta-feira"),
-    (("sábado", 5), "quinta-feira"),
-    (("sábado", 6), "sexta-feira"),
-    (("sábado", 7), "sábado"),
-    (("sábado", 0), "sábado"),
-    (("quinta-feira", 3), "domingo"),
-    (("segunda-feira", 1), "terça-feira"),
-    (("terça-feira", 0), "terça-feira"),
-    (("terça-feira", 1), "quarta-feira"),
-    (("terça-feira", 2), "quinta-feira"),
-    (("terça-feira", 3), "sexta-feira"),
-    (("terça-feira", 4), "sábado"),
-    (("terça-feira", 5), "domingo"),
-    (("terça-feira", 6), "segunda-feira"),
-    (("terça-feira", 7), "terça-feira"),
-    (("terça-feira", 180), "domingo"),
+    (('sábado', 1), 'domingo'),
+    (('sábado', 2), 'segunda-feira'),
+    (('sábado', 3), 'terça-feira'),
+    (('sábado', 4), 'quarta-feira'),
+    (('sábado', 5), 'quinta-feira'),
+    (('sábado', 6), 'sexta-feira'),
+    (('sábado', 7), 'sábado'),
+    (('sábado', 0), 'sábado'),
+    (('quinta-feira', 3), 'domingo'),
+    (('segunda-feira', 1), 'terça-feira'),
+    (('terça-feira', 0), 'terça-feira'),
+    (('terça-feira', 1), 'quarta-feira'),
+    (('terça-feira', 2), 'quinta-feira'),
+    (('terça-feira', 3), 'sexta-feira'),
+    (('terça-feira', 4), 'sábado'),
+    (('terça-feira', 5), 'domingo'),
+    (('terça-feira', 6), 'segunda-feira'),
+    (('terça-feira', 7), 'terça-feira'),
+    (('terça-feira', 180), 'domingo'),
 ]
 
 
 @pytest.mark.parametrize(
-    ["entrance", "expected"],
+    ['entrance', 'expected'],
     unittests,
 )
 def test_weekday0(entrance, expected) -> None:
@@ -40,7 +40,7 @@ def test_weekday0(entrance, expected) -> None:
 
 
 @pytest.mark.parametrize(
-    ["entrance", "expected"],
+    ['entrance', 'expected'],
     unittests,
 )
 def test_weekday(entrance, expected) -> None:
